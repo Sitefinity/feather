22d197d09aa5b4af1001390a0973898272a4812d
Desislav Bonchev <desislav.bonchev@telerik.com>
2014-05-23 13:57:54 +0300

    Mvc Helpers refactoring and unit testing

---
+++ Telerik.Sitefinity.Frontend/Resources/UrlTransformations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Telerik.Sitefinity.Frontend.Resources
+{
+    public static class UrlTransformations
+    {
+        /// <summary>
+        /// Appends a parameter to a give url.
+        /// </summary>
+        /// <param name="url">The URL.</param>
+        /// <param name="paramaterName">Name of the paramater.</param>
+        /// <param name="paramterValue">The paramter value.</param>
+        /// <returns></returns>
+        public static string AppendParam(string url, string paramaterName, string paramterValue)
+        {
+            if (paramterValue.IsNullOrEmpty())
+            {
+                return url;
+            }
+            else if (url.Contains("?"))
+            {
+                return string.Format("{0}&{1}={2}", url, paramaterName, HttpUtility.UrlEncode(paramterValue));
+            }
+            else
+            {
+                return string.Format("{0}?{1}={2}", url, paramaterName, HttpUtility.UrlEncode(paramterValue));
+            }
+        }
+    }
+}
