d38f8663dc89ffb84ab6cc5654b584cd58bed8d2
JenPeleva <zheyna.peleva@telerik.com>
2014-05-30 15:45:12 +0300

    Modified Expander.js and added styles for the navigation designer

--- Telerik.Sitefinity.Frontend/Mvc/Scripts/expander.js
+++ Telerik.Sitefinity.Frontend/Mvc/Scripts/expander.js
@@ -7,15 +7,20 @@
             replace: true,
             transclude: true,
             scope: { title: '@expanderTitle' },
-            template: '<div>' +
-                '<div class="title" ng-click="toggle()">{{title}}</div>' +
-                '<div class="body" ng-show="isExpanded" ng-transclude></div>' +
+            template: '<div class="s-more-options-wrp">' +
+                '<span class="s-title" ng-click="toggle()"><span ng-class="classExpanded" class="glyphicon glyphicon-chevron-right"></span> {{title}}</span>' +
+                '<div class="s-body" ng-show="isExpanded" ng-transclude></div>' +
                 '</div>',
             link: function (scope, element, attrs) {
                 scope.isExpanded = false;
+                scope.classExpanded = "notExpanded";
 
                 scope.toggle = function toggle() {
                     scope.isExpanded = !scope.isExpanded;
+                    if (scope.isExpanded)
+                        scope.classExpanded = "glyphicon-chevron-down";
+                    else
+                        scope.classExpanded = "glyphicon-chevron-right";
                 }
             }
         }
