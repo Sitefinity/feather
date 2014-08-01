a9c021da1862fee27d39a62dc02d07d59be04ead
Tihomir Petrov <tihomir.petrov@telerik.com>
2014-08-01 14:46:33 +0300

    All projects are updated to depend ot Sitefinity 7.1.5200.1 or higher.

--- Telerik.Sitefinity.Frontend.TestUI.Arrangements/Properties/AssemblyInfo.cs
+++ Telerik.Sitefinity.Frontend.TestUI.Arrangements/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using Telerik.Sitefinity.TestUI.Arrangements.Core.Framework.Attributes;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.1000.4")]
-[assembly: AssemblyFileVersion("0.3.1000.4")]
+[assembly: AssemblyVersion("0.4.1000.1")]
+[assembly: AssemblyFileVersion("0.4.1000.1")]
 [assembly: UIArrangement()]
