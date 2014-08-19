c87b0481ca8d9f6f860f166060d3ea2a9273d843
ElenaGaneva <Elena.Ganeva@telerik.com>
2014-08-19 15:59:56 +0300

    Removed DynamicUrlParamsResolver

--- Telerik.Sitefinity.Frontend/Mvc/Infrastructure/Controllers/ControllerContainerInitializer.cs
+++ Telerik.Sitefinity.Frontend/Mvc/Infrastructure/Controllers/ControllerContainerInitializer.cs
@@ -11,6 +11,7 @@ using Telerik.Sitefinity.Abstractions;
 using Telerik.Sitefinity.Abstractions.VirtualPath;
 using Telerik.Sitefinity.Configuration;
 using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes;
+using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Routing;
 using Telerik.Sitefinity.Frontend.Resources;
 using Telerik.Sitefinity.Frontend.Resources.Resolvers;
 using Telerik.Sitefinity.Localization;
@@ -222,10 +223,7 @@ namespace Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers
         protected virtual void InitializeCustomRouting()
         {
             //Sf 7.2: 
-            //var defaultResolver = ObjectFactory.Container.Resolve<IMvcUrlParamsResolver>();
-            //ObjectFactory.Container.RegisterType(typeof(IMvcUrlParamsResolver), defaultResolver.GetType(), DynamicUrlParamsResolver.DefaultResolverName, new ContainerControlledLifetimeManager());
-
-            //ObjectFactory.Container.RegisterType<IMvcUrlParamsResolver, DynamicUrlParamsResolver>(new ContainerControlledLifetimeManager());
+            //ObjectFactory.Container.RegisterType<IControllerActionInvoker, DynamicUrlParamActionInvoker>(new ContainerControlledLifetimeManager());
 
             //ObjectFactory.Container.RegisterType<IRouteParamResolver, CategoryParamResolver>("category");
             //ObjectFactory.Container.RegisterType<IRouteParamResolver, TagParamResolver>("tag");
---
+++ Telerik.Sitefinity.Frontend/Mvc/Infrastructure/Routing/DynamicUrlParamActionInvoker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers;
+using Telerik.Sitefinity.Mvc;
+using Telerik.Sitefinity.Mvc.Proxy;
+using Telerik.Sitefinity.Web;
+
+namespace Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Routing
+{
+    /// <summary>
+    /// Extends <see cref="Telerik.Sitefinity.Mvc.ControllerActionInvoker"/> by providing logic for custom routing.
+    /// </summary>
+    public class DynamicUrlParamActionInvoker : Telerik.Sitefinity.Mvc.ControllerActionInvoker
+    {
+        /// <inheritdoc/>
+        protected override void InitializeRouteParameters(MvcProxyBase proxyControl, RequestContext originalContext)
+        {
+            RouteHelper.SetUrlParametersResolved(false);
+
+            var urlController = proxyControl.Controller as IUrlMappingController;
+            if (urlController != null)
+            {
+                var requestContext = proxyControl.RequestContext;
+                var originalParams = MvcRequestContextBuilder.GetRouteParams(originalContext);
+                var controllerName = SitefinityViewEngine.GetControllerName(proxyControl.Controller);
+                requestContext.RouteData.Values[DynamicUrlParamActionInvoker.ControllerNameKey] = controllerName;
+                urlController.UrlParamsMapper.ResolveUrlParams(originalParams, requestContext);
+                proxyControl.Controller.TempData.Add("IsInPureMode", proxyControl.IsInPureMode);
+
+                if (!proxyControl.ContentTypeName.IsNullOrEmpty())
+                    proxyControl.Controller.RouteData.Values.Add("contentTypeName", proxyControl.ContentTypeName);
+            }
+            else
+            {
+                base.InitializeRouteParameters(proxyControl, originalContext);
+            }
+        }
+
+        /// <summary>
+        /// The controller name key
+        /// </summary>
+        public const string ControllerNameKey = "controller";
+    }
+}
