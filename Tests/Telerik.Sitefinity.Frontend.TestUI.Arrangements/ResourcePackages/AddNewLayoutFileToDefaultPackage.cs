3ca6fb3df30a683b35c5c2014f70733598e47de5
Slaveya Shtereva <slaveya.shtereva@telerik.com>
2014-08-21 15:35:17 +0300

    New arrangents for the selector UI test

--- Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/ResourcePackages/AddNewLayoutFileToDefaultPackage.cs
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/ResourcePackages/AddNewLayoutFileToDefaultPackage.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using Telerik.Sitefinity.Frontend.TestUtilities;
 using Telerik.Sitefinity.TestUI.Arrangements.Framework;
 using Telerik.Sitefinity.TestUI.Arrangements.Framework.Attributes;
 using Telerik.Sitefinity.TestUtilities.CommonOperations;
@@ -18,58 +19,30 @@ namespace Telerik.Sitefinity.Frontend.TestUI.Arrangements
         [ServerArrangement]
         public void AddNewLayoutFile()
         {
-            var assembly = this.GetArrangementsAssembly();
+            //// Path.Combine(sfpath, "ResourcePackages", packageName, "MVC", "Views", "Layouts", layoutFileName);
+
+            var assembly = FileInjectHelper.GetArrangementsAssembly();
             Stream source = assembly.GetManifestResourceStream(FileResource);
 
-            string filePath = this.GetDestinationFilePath(PackageName, LayoutFileName);
+            var path = Path.Combine("ResourcePackages",PackageName, "MVC", "Views", "Layouts", LayoutFileName);
+
+            string filePath = FileInjectHelper.GetDestinationFilePath(path);
             Stream destination = new FileStream(filePath, FileMode.Create, FileAccess.Write);
-            
-            CopyStream(source, destination);
+
+            FileInjectHelper.CopyStream(source, destination);
         }
 
         [ServerTearDown]
         public void TearDown()
         {
-            string filePath = this.GetDestinationFilePath(PackageName, LayoutFileName);
+            var path = Path.Combine("ResourcePackages", PackageName, "MVC", "Views", "Layouts", LayoutFileName);
+            string filePath = FileInjectHelper.GetDestinationFilePath(path);
 
             ServerOperations.Templates().DeletePageTemplate(TemplateTitle);
             File.Delete(filePath); 
         }
 
-        internal Assembly GetArrangementsAssembly()
-        {
-            var uiTestsArrangementsAssembly = AppDomain.CurrentDomain.GetAssemblies().Where(a => a.GetName().Name.Equals("Telerik.Sitefinity.Frontend.TestUI.Arrangements")).FirstOrDefault();
-            if (uiTestsArrangementsAssembly == null)
-            {
-                throw new DllNotFoundException("Arrangements assembly wasn't found");
-            }
-
-            return uiTestsArrangementsAssembly;
-        }
-
-        private static void CopyStream(Stream input, Stream output)
-        {
-            byte[] buffer = new byte[32768];
-            int read;
-            while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
-            {
-                output.Write(buffer, 0, read);
-            }
-        }
-
-        private string GetDestinationFilePath(string packageName, string layoutFileName)
-        {
-            var sfpath = System.Web.Hosting.HostingEnvironment.MapPath("~/");
-            var filePath = Path.Combine(sfpath, "ResourcePackages", packageName, "MVC", "Views", "Layouts", layoutFileName);
-
-            if (filePath == null)
-            {
-                throw new ArgumentNullException("FilePath doesn't exist");
-            }
-
-            return filePath;
-        }
-
+      
         private const string FileResource = "Telerik.Sitefinity.Frontend.TestUI.Arrangements.Data.TestLayout.cshtml";
         private const string PackageName = "Foundation";
         private const string LayoutFileName = "TestLayout.cshtml";
