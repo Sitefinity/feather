99b42b71e72f0cf342b602fdafc6e28facd4e9b3
PetyaRachina <petya.rachina@telerik.com>
2014-08-21 13:29:03 +0300

    edited UI test arrangement

--- Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/ResourcePackages/AddNewLayoutFileToDefaultPackage.cs
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/ResourcePackages/AddNewLayoutFileToDefaultPackage.cs
@@ -30,12 +30,7 @@ namespace Telerik.Sitefinity.Frontend.TestUI.Arrangements
         [ServerTearDown]
         public void TearDown()
         {
-            ServerOperations.SystemManager().RestartApplication(true);
-
-            string filePath = this.GetDestinationFilePath(PackageName, LayoutFileName);
-
             ServerOperations.Templates().DeletePageTemplate(TemplateTitle);
-            File.Delete(filePath); 
         }
 
         internal Assembly GetArrangementsAssembly()
