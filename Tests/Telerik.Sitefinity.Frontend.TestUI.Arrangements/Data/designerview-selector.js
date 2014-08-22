38e764da4cf1ba8eb98697ee00bbe0a8ca831ce1
Slaveya Shtereva <slaveya.shtereva@telerik.com>
2014-08-22 16:08:19 +0300

    New files for test

---
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/Data/designerview-selector.js
@@ -0,0 +1,25 @@
+﻿(function ($) {
+    var designerModule = angular.module('designer');
+
+    angular.module('designer').requires.push('selectors');
+
+    //This is basic controller for the "ManageItems" designer view.
+    designerModule.controller('SelectorCtrl', ['$scope', 'propertyService', function ($scope, propertyService) {
+        $scope.feedback.showLoadingIndicator = true;
+
+        propertyService.get()
+            .then(function (data) {
+                if (data) {
+                    $scope.properties = propertyService.toAssociativeArray(data.Items);
+                }
+            },
+            function (data) {
+                $scope.feedback.showError = true;
+                if (data)
+                    $scope.feedback.errorMessage = data.Detail;
+            })
+            .finally(function () {
+                $scope.feedback.showLoadingIndicator = false;
+            });
+    }]);
+})(jQuery);
---
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/MvcWidgets/MvcSelectorTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using Telerik.Sitefinity.Frontend.TestUI.Arrangements.MvcWidgets;
+using Telerik.Sitefinity.Frontend.TestUtilities;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework.Attributes;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace Telerik.Sitefinity.Frontend.TestUI.Arrangements
+{
+    /// <summary>
+    /// MvcWidgetDefaultFeatherDesigner arragement.
+    /// </summary>
+    public class MvcSelectorTest : ITestArrangement
+    {
+        [ServerSetUp]
+        public void SetUp()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                ServerOperations.News().CreatePublishedNewsItem(newsTitle: NewsItemTitle + i, newsContent: NewsItemContent +i, author: NewsItemAuthor + i);                
+            }
+            Guid pageId = ServerOperations.Pages().CreatePage(PageName);
+        
+            var assembly = FileInjectHelper.GetArrangementsAssembly();
+
+            ////  inject DesignerView.Selector.cshtml
+            Stream source = assembly.GetManifestResourceStream(FileResource);
+
+            var viewPath = Path.Combine("MVC", "Views", "DummyText", DesignerViewFileName);
+
+            string filePath = FileInjectHelper.GetDestinationFilePath(viewPath);
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+            Stream destination = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+
+            FileInjectHelper.CopyStream(source, destination);
+            source.Close();
+            destination.Close();
+
+            ////  inject DesignerView.Selector.json
+            Stream sourceJson = assembly.GetManifestResourceStream(FileResourceJson);
+            var jsonPath = Path.Combine("MVC", "Views", "DummyText", JsonFileName);
+
+            string filePathJson = FileInjectHelper.GetDestinationFilePath(jsonPath);
+            Directory.CreateDirectory(Path.GetDirectoryName(filePathJson));
+            Stream destinationJson = new FileStream(filePathJson, FileMode.Create, FileAccess.Write);
+
+            FileInjectHelper.CopyStream(sourceJson, destinationJson);
+            sourceJson.Close();
+            destinationJson.Close();
+
+            ////  inject designerview-selector.js
+            Stream sourceController = assembly.GetManifestResourceStream(ControllerFileResource);
+            var controllerPath = Path.Combine("MVC", "Scripts", "DummyText", ControllerFileName);
+
+            string controllerFilePath = FileInjectHelper.GetDestinationFilePath(controllerPath);
+            Directory.CreateDirectory(Path.GetDirectoryName(controllerFilePath));
+            Stream destinationController = new FileStream(controllerFilePath, FileMode.Create, FileAccess.Write);
+
+            FileInjectHelper.CopyStream(sourceController, destinationController);
+            
+            sourceController.Close();
+            destinationController.Close();            
+
+            ServerOperations.Widgets().AddMvcWidgetToPage(pageId, typeof(DummyTextController).FullName, WidgetCaption);
+        }
+
+        [ServerTearDown]
+        public void TearDown()
+        {
+            ServerOperations.Pages().DeleteAllPages();
+            ServerOperations.News().DeleteAllNews();
+
+            var path = Path.Combine("MVC", "Views", "DummyText", DesignerViewFileName);
+            string filePath = FileInjectHelper.GetDestinationFilePath(path);
+            File.Delete(filePath);
+
+            var jsonPath = Path.Combine("MVC", "Views", "DummyText", JsonFileName);
+            string filePathJson = FileInjectHelper.GetDestinationFilePath(jsonPath);
+            File.Delete(filePathJson);
+
+            var controllerPath = Path.Combine("MVC", "Scripts", "DummyText", ControllerFileName);
+            string controllerFilePath = FileInjectHelper.GetDestinationFilePath(controllerPath);
+            File.Delete(controllerFilePath);
+        }
+
+        private const string FileResource = "Telerik.Sitefinity.Frontend.TestUI.Arrangements.Data.DesignerView.Selector.cshtml";
+        private const string FileResourceJson = "Telerik.Sitefinity.Frontend.TestUI.Arrangements.Data.DesignerView.Selector.json";
+        private const string ControllerFileResource = "Telerik.Sitefinity.Frontend.TestUI.Arrangements.Data.designerview-selector.js";
+
+        private const string DesignerViewFileName = "DesignerView.Selector.cshtml";
+        private const string JsonFileName = "DesignerView.Selector.json";
+        private const string ControllerFileName = "designerview-selector.js";
+
+        private const string PageName = "FeatherPage";
+        private const string WidgetCaption = "SelectorWidget";
+
+        private const string NewsItemTitle = "News Item Title";
+        private const string NewsItemContent = "This is a news item.";
+        private const string NewsItemAuthor = "NewsWriter";
+    }
+}
