4502f988f4e5a353288ebfba21d4e6cdb2e6c012
GeorgiMateev <georgi.mateev@telerik.com>
2014-08-26 20:05:58 +0300

    Added test for two modal dialogs

--- Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/Mvc/Scripts/Designer/modalDialogSpec.js
+++ Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/Mvc/Scripts/Designer/modalDialogSpec.js
@@ -6,6 +6,7 @@ describe('modal directive', function () {
     var $modal = {
         open: jasmine.createSpy('$modal.open').andReturn(dialogResult)
     };
+    var dialogsService;
 
     beforeEach(module('modalDialog'));
 
@@ -13,24 +14,28 @@ describe('modal directive', function () {
         $provide.value('$modal', $modal);
     }));
 
-    beforeEach(inject(function ($rootScope, $q) {
+    beforeEach(inject(function ($rootScope, $q, _dialogsService_) {
         scope = $rootScope.$new();
         scope.test = 'test prop';
+
+        dialogsService = _dialogsService_;
 
         dialogResultDefer = $q.defer();
         dialogResult.result = dialogResultDefer.promise;
     }));
 
     afterEach(function () {
-        $('#testDiv').empty();
-        $('#testDiv').remove();
+        $('.testDiv').empty();
+        $('.testDiv').remove();
     });
 
     /* Helper methods */
-    var compileDirective = function (template) {
+    var compileDirective = function (template, container) {
+        var cntr = container || 'body';
+
         inject(function ($compile) {
             directiveElement = $compile(template)(scope);
-            $('body').append($('<div/>').attr('id', 'testDiv')
+            $(cntr).append($('<div/>').addClass('testDiv')
                 .append(directiveElement));
         });
         
@@ -38,7 +43,9 @@ describe('modal directive', function () {
         scope.$digest();
     }
 
-    var assertOpenWithController = function () {
+    var assertOpenWithController = function (templateId, skipClean) {
+        var tmplId = templateId || 'dialog-template';
+
         var mostRecent = $modal.open.mostRecentCall;
         expect(mostRecent).toBeDefined();
         expect(mostRecent.args).toBeDefined();
@@ -47,10 +54,12 @@ describe('modal directive', function () {
 
         expect(args.backdrop).toBe('static');
         expect(args.scope).toBeUndefined();
-        expect(args.templateUrl).toBe('dialog-template');
+        expect(args.templateUrl).toBe(tmplId);
         expect(args.controller).toBe('TestCtrl');
 
-        $modal.open.reset();
+        if (!skipClean) {
+            $modal.open.reset();
+        }
     }
 
     var assertOpenWithScope = function () {
@@ -91,7 +100,7 @@ describe('modal directive', function () {
                 '<div class="modal-body">' +
                     '<h2>Body</h2>' +
                 '</div>' +
-            '</script>'
+            '</script>' +
         '</div>';
 
         compileDirective(template);
@@ -107,14 +116,48 @@ describe('modal directive', function () {
                 '<div class="modal-body">' +
                     '<h2>Body</h2>' +
                 '</div>' +
-            '</script>'
+            '</script>' +
         '</div>';
 
         compileDirective(template);
 
-        expect($modal.open).not.toHaveBeenCalled()
+        expect($modal.open).not.toHaveBeenCalled();
 
         $('#openSelectorBtn').click();
+
+        assertOpenWithScope();
+    });
+
+    it('[GMateev] / should open two nested dialogs.]', function () {
+        var template =
+        '<div class="designer" modal dialog-controller="TestCtrl" template-url="outside-dialog-template" auto-open="true">' +
+            '<script type="text/ng-template" id="outside-dialog-template">' +
+               '<div id="dialogWrapper"></div>' +
+            '</script>' +
+        '</div>';
+
+        var template2 = '<button id="openSelectorBtn">Open</button>' +
+                '<div class="designer" modal existing-scope="true" template-url="dialog-template" open-button="#openSelectorBtn">' +
+                    '<script type="text/ng-template" id="dialog-template">' +
+                        '<div class="modal-body">' +
+                            '<h2>Body</h2>' +
+                        '</div>' +
+                    '</script>' +
+                '</div>';
+
+        compileDirective(template);        
+
+        expect($modal.open.callCount).toBe(1);
+        expect(dialogsService.getOpenedDialogsCount()).toBe(1);
+
+        assertOpenWithController("outside-dialog-template", true);
+
+        compileDirective(template2);
+
+        $('#openSelectorBtn').click();
+
+        expect($modal.open.callCount).toBe(2);
+        expect(dialogsService.getOpenedDialogsCount()).toBe(2);
 
         assertOpenWithScope();
     });
