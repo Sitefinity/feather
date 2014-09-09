da464d40d54af217e471c7f79214a20d077d805a
Boyko-Karadzhov <boyko.karadzhov@telerik.com>
2014-09-09 10:38:46 +0300

    directive templates are now in embedded files

--- Telerik.Sitefinity.Frontend/Mvc/Scripts/expander.js
+++ Telerik.Sitefinity.Frontend/Mvc/Scripts/expander.js
@@ -6,20 +6,17 @@
 				replace: true,
 				transclude: true,
 				scope: { title: '@expanderTitle' },
-				template: '<div class="s-more-options-wrp">' +
-					'<span class="s-title" ng-click="toggle()"><span ng-class="classExpanded" class="glyphicon glyphicon-chevron-right"></span> {{title}}</span>' +
-					'<div class="s-body" ng-show="isExpanded" ng-transclude></div>' +
-					'</div>',
+				templateUrl: sitefinity.getEmbeddedResourceUrl('Telerik.Sitefinity.Frontend', 'Mvc/Scripts/Templates/expander.html'),
 				link: function (scope, element, attrs) {
 					scope.isExpanded = false;
-					scope.classExpanded = "notExpanded";
+					scope.classExpanded = 'notExpanded';
 
 					scope.toggle = function toggle() {
 						scope.isExpanded = !scope.isExpanded;
 						if (scope.isExpanded)
-							scope.classExpanded = "glyphicon-chevron-down";
+							scope.classExpanded = 'glyphicon-chevron-down';
 						else
-							scope.classExpanded = "glyphicon-chevron-right";
+							scope.classExpanded = 'glyphicon-chevron-right';
 					};
 				}
 			};
