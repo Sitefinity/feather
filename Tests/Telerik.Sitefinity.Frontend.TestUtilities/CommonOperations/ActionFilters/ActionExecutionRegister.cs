fbad0f1daeff1a280dd95646db55dc6927b49f4d
ElenaGaneva <Elena.Ganeva@telerik.com>
2014-09-17 15:42:23 +0300

    Added integration tests for referencing scripts functionality.

---
+++ Tests/Telerik.Sitefinity.Frontend.TestUtilities/CommonOperations/ActionFilters/ActionExecutionRegister.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Telerik.Sitefinity.Frontend.TestUtilities.CommonOperations.ActionFilters
+{
+    /// <summary>
+    /// This class represents register for executed actions.
+    /// </summary>
+    public static class ActionExecutionRegister
+    {
+        /// <summary>
+        /// Gets the executed action information.
+        /// </summary>
+        /// <value>
+        /// The executed action information.
+        /// </value>
+        public static IList<ActionInfo> ExecutedActionInfos
+        {
+            get
+            {
+                return ActionExecutionRegister.executedActionInfos;
+            }
+        }
+
+        private static IList<ActionInfo> executedActionInfos = new List<ActionInfo>();
+    }
+}
---
+++ Tests/Telerik.Sitefinity.Frontend.TestUtilities/CommonOperations/ActionFilters/ExecutionRegistrationFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Telerik.Sitefinity.Frontend.TestUtilities.CommonOperations.ActionFilters
+{
+    /// <summary>
+    /// This class represents filter attribute which registers the execution of every action.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1813:AvoidUnsealedAttributes"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1018:MarkAttributesWithAttributeUsage")]
+    public class ExecutionRegistrationFilterAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Called by the ASP.NET MVC framework after the action result executes.
+        /// </summary>
+        /// <param name="filterContext">The filter context.</param>
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            base.OnResultExecuted(filterContext);
+            
+            var actionInfo = new ActionInfo();
+            actionInfo.Name = filterContext.RouteData.Values["action"].ToString();
+            actionInfo.ActionRouteData = filterContext.RouteData;
+            actionInfo.Result = filterContext.Result;
+            actionInfo.CurrentHttpContext = filterContext.HttpContext;
+            ActionExecutionRegister.ExecutedActionInfos.Add(actionInfo);
+        }
+    }
+}
