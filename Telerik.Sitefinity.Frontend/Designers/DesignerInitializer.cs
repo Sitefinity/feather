d175b69326c804faccce90abdaedbf28505113e1
Boyko-Karadzhov <boyko.karadzhov@telerik.com>
2014-09-19 16:00:40 +0300

    additional designer scripts are now loaded with LABjs

--- Telerik.Sitefinity.Frontend/Designers/DesignerInitializer.cs
+++ Telerik.Sitefinity.Frontend/Designers/DesignerInitializer.cs
@@ -62,6 +62,8 @@ namespace Telerik.Sitefinity.Frontend.Designers
                 @event.Scripts.Add(new ScriptReference(scriptRootPath + "Designers/Scripts/page-editor.js"));
                 @event.Scripts.Add(new ScriptReference(scriptRootPath + "Mvc/Scripts/Kendo/angular-kendo.js"));
 
+                @event.Scripts.Add(new ScriptReference(scriptRootPath + "Mvc/Scripts/LABjs/LAB.min.js"));
+
                 var currentPackage = new PackageManager().GetCurrentPackage();
                 if (!currentPackage.IsNullOrEmpty())
                 {
