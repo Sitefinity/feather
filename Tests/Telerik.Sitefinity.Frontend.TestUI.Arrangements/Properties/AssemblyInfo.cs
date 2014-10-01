8acb58e2ff0b8b4e82bfaed3fb23981d0112a5e7
Tihomir Petrov <tihomir.petrov@telerik.com>
2014-10-01 16:24:41 +0300

    The assembly version for all projects is updated.

--- Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/Properties/AssemblyInfo.cs
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using Telerik.Sitefinity.TestUI.Arrangements.Core.Framework.Attributes;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.10.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: UIArrangement]
 [assembly: ControllerContainer] 
