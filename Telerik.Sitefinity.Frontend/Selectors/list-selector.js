57af6c7f04cf13e62b06f5df478741722a9bb917
GeorgiMateev <georgi.mateev@telerik.com>
2014-10-01 18:39:43 +0300

    Implemented dynamic items selector

--- Telerik.Sitefinity.Frontend/Selectors/list-selector.js
+++ Telerik.Sitefinity.Frontend/Selectors/list-selector.js
@@ -8,9 +8,27 @@
                     selectedItemId: '=?',
                     selectedItem: '=?',
                     provider: '=?', /* content-selector */
-                    taxonomyId: '=?' /* taxon-selector */
+                    taxonomyId: '=?', /* taxon-selector */
+                    itemType: '=?', /* dynamic-items-selector */
+                    identifierField: '=?'
                 },
                 controller: function ($scope) {
+                    this.defaultIdentifierField = 'Title';
+
+                    this.identifierField = $scope.identifierField || this.defaultIdentifierField;
+
+                    this.bindIdentifierField = function (item) {
+                        if (item) {
+                            var mainField = item[this.identifierField];
+                            if (mainField) {
+                                return mainField;
+                            }
+                            else {
+                                return item.Id;
+                            }
+                        }
+                    };
+
                     this.getSelectedItemId = function () {
                         return $scope.selectedItemId;
                     };
@@ -26,6 +44,10 @@
                     this.getProvider = function () {
                         return $scope.provider;
                     };
+
+                    this.getItemType = function () {
+                        return $scope.itemType;
+                    }
 
                     this.updateSelectedItem = function (selectedItem) {
                         $scope.selectedItem = selectedItem;
@@ -110,8 +132,12 @@
 
                         var onError = function (error) {
                             var errorMessage = '';
-                            if (error && error.data.ResponseStatus)
+                            if (error && error.data.ResponseStatus) {
                                 errorMessage = error.data.ResponseStatus.Message;
+                            }
+                            else if (error && error.statusText) {
+                                errorMessage = error.statusText;
+                            }
 
                             scope.showError = true;
                             scope.errorMessage = errorMessage;
@@ -121,6 +147,12 @@
                         // helper methods
                         // ------------------------------------------------------------------------
 
+                        var emptyGuid = '00000000-0000-0000-0000-000000000000';
+
+                        var selectedId = function () {
+                            return (scope.selectedItem && scope.selectedItem.Id) || scope.selectedItemId;
+                        };
+
                         var loadItems = function () {
                             var skip = scope.filter.paging.totalItems;
                             var take = scope.filter.paging.itemsPerPage;
@@ -129,12 +161,12 @@
                                 .then(onItemsLoadedSuccess, onError);
                         };
 
-                        var getSelectedItem = function (scope) {
-                            var id = (scope.selectedItem && scope.selectedItem.Id) || scope.selectedItemId;
+                        var getSelectedItem = function () {
+                            var id = selectedId();
 
-                            if (id && id !== '00000000-0000-0000-0000-000000000000') {
+                            if (id && id !== emptyGuid) {
                                 ctrl.getItem(id)
-                                    .then(ctrl.onSelectedItemLoadedSuccess);
+                                    .then(ctrl.onSelectedItemLoadedSuccess, onError);
                             }
                         };
 
@@ -153,21 +185,11 @@
                         };
 
                         var isFilterApplied = function () {
-                            if (scope.filter.search && scope.filter.search !== '') {
-                                return true;
-                            }
-                            else {
-                                return false;
-                            }
+                            return scope.filter.search && scope.filter.search !== '';
                         };
 
                         var isCurrentItemSelected = function (id) {
-                            if (id === scope.selectedItemId || (scope.selectedItem && id === scope.selectedItem.Id)) {
-                                return true;
-                            }
-                            else {
-                                return false;
-                            }
+                            return id === selectedId();
                         };
 
                         // ------------------------------------------------------------------------
@@ -281,19 +303,19 @@
                         };
 
                         scope.isItemSelected = function () {
-                            if (scope.selectedItemId && scope.selectedItemId !== '00000000-0000-0000-0000-000000000000') {
-                                return true;
-                            }
-                            else {
-                                return false;
-                            }
+                            var id = selectedId();
+                            return id && id !== emptyGuid;
                         };
 
                         scope.showLoadingIndicator = true;
 
                         scope.hideEndlessScrollLoadingIndicator = false;
 
-                        getSelectedItem(scope);
+                        scope.bindIdentifierField = function (item) {
+                            return ctrl.bindIdentifierField(item);
+                        };
+
+                        getSelectedItem();
 
                         transclude(scope, function (clone) {
                             var hasContent;
