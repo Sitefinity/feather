a1e7aada6f0934d8d1d255d331a83021648c3735
GeorgiMateev <georgi.mateev@telerik.com>
2014-10-20 19:23:29 +0300

    Added files for page selector

---
+++ Telerik.Sitefinity.Frontend/Selectors/items-tree.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    angular.module('selectors')
+        .directive('sfItemsTree', function () {
+            return {
+                restrict: 'E',
+                scope: {
+                    sfItems: '=',
+                    sfItemSelected: '&',
+                    sfGetItems: '&'
+                },
+                link: function (scope, element, attrs) {
+                    scope.itemsDataSource = new kendo.data.HierarchicalDataSource({
+                        data: scope.sfItems,
+                        schema: {
+                            model: {
+                                hasChildren: "HasChildren",
+                            }
+                        },
+                        select: function (e) {
+                            alert(e);
+                        }
+                    });
+
+                    scope.expand = function (e) {
+                        var dataItem = e.sender.dataItem(e.node);
+                        if (dataItem && dataItem.Id) {
+                            scope.sfGetItems(dataItem.id)
+                                .then(function (data) {
+                                    dataItem.items = [];
+                                    Array.prototype.push.apply(dataItem.items, data);
+                                    //angular.forEach(data, function (item, _) {
+                                    //    dataItem.items.push(item);
+                                    //});
+                                });
+                        }
+                    };
+
+                    scope.change = function (dataItem) {
+                        //TODO: check if this method is actually needed.
+                        scope.sfItemSelected(dataItem);
+                    };
+
+                    scope.itemTemplate = "<span style='cursor:hand'>{{dataItem.Title}}</span>";
+                }
+            }
+        });
+})();
---
+++ Telerik.Sitefinity.Frontend/Selectors/page-selector.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    angular.module('selectors')
+        .directive('sfPageSelector', ['sfPageService', function (pageService) {
+            return {
+                require: '^listSelector',
+                restrict: 'A',
+                link: {
+                    pre: function (scope, element, attrs, ctrl) {
+                        ctrl.getChildItems = function (parentId, search) {
+                            var siteId = ctrl.$scope.siteId;
+                            var provider = ctrl.$scope.provider;
+                            return pageService.getItems(parentId, siteId, provider, search);
+                        };
+
+                        ctrl.getSpecificItems = function (ids) {
+                            //var provider = ctrl.$scope.provider;
+                            //return pageService.getSpecificItems(ids, provider);
+                        };
+
+                        ctrl.onSelectedItemsLoadedSuccess = function (data) {
+                            ctrl.updateSelection(data.Items);
+                        };
+
+                        ctrl.selectorType = 'PageSelector';
+                        ctrl.templateUrl = 'Selectors/page-selector.html';
+                        ctrl.$scope.partialTemplate = 'page-selector-template';
+                    }
+                }
+            };
+        }]);
+})();
