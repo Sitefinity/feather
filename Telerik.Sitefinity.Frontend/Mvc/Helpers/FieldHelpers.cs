c6e3b78938fb96520396fbd8ffab25d61becf4be
ElenaGaneva <Elena.Ganeva@telerik.com>
2014-11-11 19:03:35 +0200

    Added related data inline list templates

--- Telerik.Sitefinity.Frontend/Mvc/Helpers/FieldHelpers.cs
+++ Telerik.Sitefinity.Frontend/Mvc/Helpers/FieldHelpers.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Web.Mvc;
 using Telerik.Sitefinity.Frontend.Mvc.Helpers.ViewModels.Fields;
+using Telerik.Sitefinity.Model;
 
 namespace Telerik.Sitefinity.Frontend.Mvc.Helpers
 {
@@ -22,7 +23,7 @@ namespace Telerik.Sitefinity.Frontend.Mvc.Helpers
         {
             helper.ViewBag.CssClass = cssClass;
 
-            return ASP.PartialExtensions.Partial(helper, "LongTextAreaField", text);
+            return ASP.PartialExtensions.Partial(helper, FieldHelpers.TextAreaFieldViewName, text);
         }
 
         /// <summary>
@@ -43,11 +44,33 @@ namespace Telerik.Sitefinity.Frontend.Mvc.Helpers
             var model = new ClassificationFieldViewModel((IList<Guid>)classificationFieldValue, classificationId, fieldTitle, fieldName);
 
             if (model.GetTaxonomyType() == Telerik.Sitefinity.Taxonomies.Model.TaxonomyType.Flat)
-                return ASP.PartialExtensions.Partial(helper, "FlatTaxonomyField", model);
+                return ASP.PartialExtensions.Partial(helper, FieldHelpers.FlatTaxonomyFieldViewName, model);
             else if (model.GetTaxonomyType() == Telerik.Sitefinity.Taxonomies.Model.TaxonomyType.Hierarchical)
-                return ASP.PartialExtensions.Partial(helper, "HierarchicalTaxonomyField", model);
+                return ASP.PartialExtensions.Partial(helper, FieldHelpers.HierarchicalTaxonomyFieldViewName, model);
 
             return null;
         }
+
+        /// <summary>
+        /// Renders related data inline list field.
+        /// </summary>
+        /// <param name="helper">The helper.</param>
+        /// <param name="relatedDataItems">The related data items.</param>
+        /// <param name="identifierField">The identifier field.</param>
+        /// <param name="frontendWidgetLabel">The frontend widget label.</param>
+        /// <param name="cssClass">The CSS class.</param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed")]
+        public static System.Web.Mvc.MvcHtmlString RelatedDataInlineListField(this HtmlHelper helper, IList<IDataItem> relatedDataItems, string identifierField, string frontendWidgetLabel, string cssClass = "")
+        {
+            helper.ViewBag.CssClass = cssClass;
+            var model = new RelatedDataFieldViewModel(relatedDataItems, frontendWidgetLabel, identifierField);
+
+            return ASP.PartialExtensions.Partial(helper, "RelatedDataInlineListField", model);
+        }
+
+        private const string TextAreaFieldViewName = "LongTextAreaField";
+        private const string FlatTaxonomyFieldViewName = "FlatTaxonomyField";
+        private const string HierarchicalTaxonomyFieldViewName = "HierarchicalTaxonomyField";
     }
 }
