65044c30697f53741b1d2239a3543931bf3302be
PetyaRachina <petya.rachina@telerik.com>
2014-12-02 10:06:35 +0200

    fixed WidgetsWrapper

--- Tests/Telerik.Sitefinity.Frontend.TestUI.Framework/Framework/Wrappers/Backend/Widgets/WidgetsWrapper.cs
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.Framework/Framework/Wrappers/Backend/Widgets/WidgetsWrapper.cs
@@ -471,9 +471,7 @@ namespace Telerik.Sitefinity.Frontend.TestUI.Framework.Wrappers.Backend
         private bool WaitForSaveButton()
         {
             Manager.Current.ActiveBrowser.RefreshDomTree();
-            var saveButton = ActiveBrowser.Find.ByExpression<HtmlButton>("tagname=button", "class=btn btn-primary pull-left ng-scope");
-
-            saveButton.Wait.ForExists();
+            var saveButton = this.EM.Widgets.FeatherWidget.SaveButton;
 
             bool result = saveButton != null && saveButton.IsVisible();
 
