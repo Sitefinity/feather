9e1e06b3c512bdc88ee3c6c8ffc9aeafc44ba8e2
PetyaRachina <petya.rachina@telerik.com>
2015-01-09 14:10:11 +0200

    updated feather version

--- Telerik.Sitefinity.Frontend/Properties/AssemblyInfo.cs
+++ Telerik.Sitefinity.Frontend/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.20.3")]
-[assembly: AssemblyFileVersion("1.1.20.3")]
+[assembly: AssemblyVersion("1.1.30.1")]
+[assembly: AssemblyFileVersion("1.1.30.1")]
 
 [assembly: ControllerContainer]
 
