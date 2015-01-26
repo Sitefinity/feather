43ecde2f666fce451555822247db8dd78800d40c
Boyko Karadzhov <bokovskii@gmail.com>
2015-01-26 16:14:19 +0200

    added selected items spec in sf-collection

--- Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/Selectors/sfCollectionSpec.js
+++ Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/Selectors/sfCollectionSpec.js
@@ -20,5 +20,28 @@
         commonMethods.compileDirective(directiveMarkup, scope);
 
         expect($('.sfCollectionItem').length).toEqual(5);
+
+        for (var i = 1; i <= 5; i++)
+            expect($('.sfCollectionItem span:contains(' + i + ')').length).toEqual(1);
+    });
+
+    it('[Boyko-Karadzhov] / should mark the selected items with sf-selected class.', function () {
+        var scope = $rootScope.$new();
+        scope.template = '<li ng-repeat="item in items" sf-identifier="id" class="sfCollectionItem"><div><span>Id:</span><span>{{item.id}}</span></div><div>{{item.Title}}</div></li>';
+        scope.dataItems = [];
+        for (var i = 1; i <= 5; i++) {
+            scope.dataItems.push({
+                id: i,
+                title: 'Item ' + i
+            });
+        }
+
+        scope.selectedItems = [2, 4];
+
+        var directiveMarkup = '<sf-collection sf-template="template" sf-data="dataItems" ng-model="selectedItems"></sf-collection>';
+        commonMethods.compileDirective(directiveMarkup, scope);
+        expect($('.sfCollectionItem.sf-selected').length).toEqual(2);
+        expect($('.sfCollectionItem.sf-selected span:contains(2)').length).toEqual(1);
+        expect($('.sfCollectionItem.sf-selected span:contains(4)').length).toEqual(1);
     });
 });
