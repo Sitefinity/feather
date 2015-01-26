218eb7f7d7d82f5a50d9d3e5347c392a3bc4cf8d
dzhenko <dzhenko@gmail.com>
2015-01-26 16:05:45 +0200

    Reverted unintended changes to sf-list-selector.js.

--- Telerik.Sitefinity.Frontend/client-components/selectors/common/sf-collection.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/common/sf-collection.js
@@ -1,1 +1,16 @@
-﻿
+﻿; (function ($) {
+    angular.module('sfSelectors')
+        .directive('sfListSelector', [function () {
+            return {
+                restrict: 'A',
+                transclude: false,
+                scope: {
+                    sfData: '=?',
+                    sfLoadMore: '=?'
+                },
+                link: function ($scope, element, attr, ctrl) {
+
+                }
+            };
+        }]);
+})(jQuery);
