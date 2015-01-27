b737c7a8d2a832de13a86e3ff1fb3375e28e4bc1
dzhenko <dzhenko@gmail.com>
2015-01-27 11:40:43 +0200

    Added isSelected method.

--- Telerik.Sitefinity.Frontend/client-components/selectors/common/sf-collection.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/common/sf-collection.js
@@ -8,12 +8,13 @@
                     sfMultiselect: '@',
                     items: '=sfData',
                     ngModel: '=',
+                    sfIdentifier: '@',
                     selectedItems: '=ngModel',
                     sfLoadMore: '&'
                 },
                 templateUrl: function (elem, attrs) {
                     if (!attrs.sfTemplateUrl) {
-                        throw { message: "You can not provide both template html and template url." };
+                        throw { message: "You must provide template url." };
                     }
 
                     var assembly = attrs.sfTemplateAssembly || 'Telerik.Sitefinity.Frontend';
@@ -21,27 +22,53 @@
                 },
                 link: {
                     pre: function (scope, element, attrs, ctrl) {
+                        scope.isSelected = function (item) {
+                            if (scope.selectedItems === undefined) {
+                                return false;
+                            }
+
+                            return getItemIndex(item) >= 0;
+                        };
                         scope.selectItem = function (item) {
-                            if (scope.sfMultiselect) {
-                                var itemIndex = scope.selectedItems.indexOf(id);
+                            if (scope.selectedItems === undefined) {
+                                return;
+                            }
+
+                            var itemIndex = getItemIndex(item);
+
+                            if (scope.sfMultiselect === undefined) {
                                 if (itemIndex < 0) {
-                                    scope.selectedItems.push(id);
+                                    scope.selectedItems = [item];
+                                }
+                                else {
+                                    scope.selectedItems = [];
+                                }
+                            }
+                            else {
+                                if (itemIndex < 0) {
+                                    scope.selectedItems.push(item);
                                 }
                                 else {
                                     scope.selectedItems.splice(itemIndex, 1);
                                 }
                             }
-                            else {
-                                scope.selectedItems = [id];
-                            }
-
-                            ctrl.$setViewValue(scope.selectedItems);
-                            ctrl.$apply();
                         };
 
-                        //$scope.$on("$destroy", function () {
+                        var getItemIndex = function (item) {
+                            if (scope.selectedItems === undefined) {
+                                return;
+                            }
 
-                        //});
+                            var prop = scope.sfIdentifier || 'Id';
+
+                            for (var i = 0; i < scope.selectedItems.length; i++) {
+                                if (scope.selectedItems[i][prop] === item[prop]) {
+                                    return i;
+                                }
+                            }
+
+                            return -1;
+                        };
                     }
                 }
             };
