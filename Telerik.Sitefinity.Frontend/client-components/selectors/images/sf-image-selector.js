e3806b2f04342a154fd70baae382456ee3d92764
Boyko Karadzhov <bokovskii@gmail.com>
2015-01-28 15:13:23 +0200

    image selector filter object

--- Telerik.Sitefinity.Frontend/client-components/selectors/images/sf-image-selector.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/images/sf-image-selector.js
@@ -3,10 +3,41 @@
     sfSelectors.requires.push('sfInfiniteScroll');
 
     sfSelectors
-        .directive('sfImageSelector', ['serverContext', 'sfImageService', function (serverContext, sfImageService) {
+        .directive('sfImageSelector', ['serverContext', 'sfImageService', 'serviceHelper', function (serverContext, sfImageService, serviceHelper) {
             var constants = {
                 initialLoadedItemsCount: 50,
                 infiniteScrollLoadedItemsCount: 20,
+            };
+            
+            var TaxonFilterObject = function () {
+                this.id = null;
+                this.field = null;
+
+                this.expression = function () {
+                    return this.field + '.Contains(' + this.id + ')';
+                };
+            };
+
+            var FilterObject = function () {
+                // Query that is typed by a user in a text box.
+                this.query = null;
+
+                // Recent, My or AllLibraries
+                this.basic = null;
+
+                // Parent id
+                this.parent = null;
+
+                // Last 1 day, Last 3 days, etc...
+                this.date = null;
+
+                // Filter by any taxon
+                this.taxon = null;
+
+                this.expression = function () {
+                    return serviceHelper.filterBuilder()
+                                .lifecycleFilter();
+                };
             };
 
             return {
@@ -20,7 +51,7 @@
                     return serverContext.getEmbeddedResourceUrl(assembly, url);
                 },
                 link: function (scope, element, attrs, ctrl) {
-                    scope.filterObject = {};
+                    scope.filterObject = new FilterObject();
                     scope.sortExpression = null;
                     scope.items = [];
 
