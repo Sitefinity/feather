2ff6cf7593dbe3787bad5d8abab4a19253ccc4f0
dzhenko <dzhenko@gmail.com>
2015-01-28 14:56:03 +0200

    Initial image selector structure.

--- Telerik.Sitefinity.Frontend/client-components/selectors/images/sf-image-selector.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/images/sf-image-selector.js
@@ -3,26 +3,54 @@
     sfSelectors.requires.push('sfInfiniteScroll');
 
     sfSelectors
-        .directive('sfImageSelector', ['serverContext', function (serverContext) {
+        .directive('sfImageSelector', ['serverContext', 'sfImageService', function (serverContext, sfImageService) {
+            var constants = {
+                initialLoadedItemsCount: 50,
+                infiniteScrollLoadedItemsCount: 20,
+            };
+
             return {
                 restrict: 'E',
+                scope: {
+                    selectedItem: '=ngModel'
+                },
                 templateUrl: function (elem, attrs) {
                     var assembly = attrs.sfTemplateAssembly || 'Telerik.Sitefinity.Frontend';
                     var url = attrs.sfTemplateUrl || 'client-components/selectors/images/sf-image-selector.html';
                     return serverContext.getEmbeddedResourceUrl(assembly, url);
                 },
                 link: function (scope, element, attrs, ctrl) {
-                    var loadingStep = 10;
+                    scope.filterObject = {};
+                    scope.sortExpression = null;
                     scope.items = [];
 
+                    var filterExpression = null;
+
+                    // initial open populates dialog with all root libraries
+                    sfImageService.getFolders({ take: constants.initialLoadedItemsCount }).then(function (rootFolders) {
+                        scope.items = rootFolders;
+                    });
+
                     scope.loadMore = function () {
-                        for (var i = 0; i < loadingStep; i++) {
-                            scope.items.push({ Id: scope.items.length + 1 });
-                        }
+
                     };
 
-                    scope.loadMore();
-                    scope.loadMore();
+                    scope.$watch('filterObject', function (newVal, oldVal) {
+                        filterExpression = composeFilter(newVal);
+                        refresh();
+                    }, true);
+
+                    scope.$watch('sortExpression', function (newVal, oldVal) {
+                        refresh();
+                    }, true);
+
+                    var refresh = function () {
+
+                    };
+
+                    var composeFilter = function (filterObject) {
+
+                    };
                 }
             };
         }]);
