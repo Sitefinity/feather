9efc8a33cb9ed6a4d4cf788978a0073970e2b179
Boyko Karadzhov <bokovskii@gmail.com>
2015-01-28 13:37:44 +0200

    image selector initial commit

---
+++ Telerik.Sitefinity.Frontend/client-components/selectors/images/sf-image-selector.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    var sfSelectors = angular.module('sfSelectors');
+    sfSelectors.requires.push('sfInfiniteScroll');
+
+    sfSelectors
+        .directive('sfImageSelector', ['serverContext', function (serverContext) {
+            return {
+                restrict: 'E',
+                templateUrl: function (elem, attrs) {
+                    var assembly = attrs.sfTemplateAssembly || 'Telerik.Sitefinity.Frontend';
+                    var url = attrs.sfTemplateUrl || 'client-components/selectors/images/sf-image-selector.html';
+                    return serverContext.getEmbeddedResourceUrl(assembly, url);
+                },
+                link: function (scope, element, attrs, ctrl) {
+                    var loadingStep = 10;
+                    scope.items = [];
+
+                    scope.loadMore = function () {
+                        for (var i = 0; i < loadingStep; i++) {
+                            scope.items.push({ Id: scope.items.length + 1 });
+                        }
+                    };
+
+                    scope.loadMore();
+                    scope.loadMore();
+                }
+            };
+        }]);
+})();
