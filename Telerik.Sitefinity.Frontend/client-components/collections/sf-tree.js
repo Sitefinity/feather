86f5cdf4c6a7f7a6e54a71bdf131d3d570f4e84f
dzhenko <dzhenko@gmail.com>
2015-01-29 16:14:12 +0200

    initial expand tree function.

--- Telerik.Sitefinity.Frontend/client-components/collections/sf-tree.js
+++ Telerik.Sitefinity.Frontend/client-components/collections/sf-tree.js
@@ -2,9 +2,12 @@
     angular.module('feather')
         .directive('sfTree', ['serverContext', function (serverContext) {
             return {
-                restrict: 'A',
+                restrict: 'AE',
                 scope: {
-
+                    selectedId: '=?ngModel',
+                    sfIdentifier: '@',
+                    sfParentIdentifier: '@',
+                    sfRequestChildren: '^&'
                 },
                 templateUrl: function (elem, attrs) {
                     var assembly = attrs.sfTemplateAssembly || 'Telerik.Sitefinity.Frontend';
@@ -13,7 +16,19 @@
                 },
                 link: {
                     pre: function (scope, element, attrs, ctrl) {
-                        
+                        scope.sfIdentifier = scope.sfIdentifier || 'Id';
+                        scope.sfParentIdentifier = scope.sfParentIdentifier || 'ParentId';
+
+                        scope.hierarchy = {};
+
+                        scope.expandTree = function (parentId) {
+                            scope.sfRequestChildren(parentId).then(function (data) {
+                                // populate hierhy
+                            });
+                        }
+
+                        // Initial load of root elements
+                        scope.expandTree(null);
                     }
                 }
             };
