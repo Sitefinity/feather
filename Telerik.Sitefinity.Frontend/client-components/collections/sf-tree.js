c969f760a69170a1f074352a66f4518cbd765923
dzhenko <dzhenko@gmail.com>
2015-01-29 15:47:14 +0200

    Added initial structure of sf-tree.

---
+++ Telerik.Sitefinity.Frontend/client-components/collections/sf-tree.js
@@ -0,0 +1,21 @@
+﻿; (function ($) {
+    angular.module('feather')
+        .directive('sfTree', ['serverContext', function (serverContext) {
+            return {
+                restrict: 'A',
+                scope: {
+
+                },
+                templateUrl: function (elem, attrs) {
+                    var assembly = attrs.sfTemplateAssembly || 'Telerik.Sitefinity.Frontend';
+                    var url = attrs.sfItemTemplateUrl || 'client-components/collections/sf-tree-item.html';
+                    return serverContext.getEmbeddedResourceUrl(assembly, attrs.sfItemTemplateUrl);
+                },
+                link: {
+                    pre: function (scope, element, attrs, ctrl) {
+                        
+                    }
+                }
+            };
+        }]);
+})(jQuery);
