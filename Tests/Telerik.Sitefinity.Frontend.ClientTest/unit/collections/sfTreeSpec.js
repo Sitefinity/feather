cc58e31fdeb991e2e560bb906d9411c372358b08
Boyko Karadzhov <bokovskii@gmail.com>
2015-01-29 18:28:59 +0200

    added more specs for sf-tree

--- Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/collections/sfTreeSpec.js
+++ Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/collections/sfTreeSpec.js
@@ -1,27 +1,69 @@
 ﻿describe('tree directive', function () {
     beforeEach('sfTree');
 
-    var rootScope;
+    var $rootScope;
+    var $q;
     var templateCache;
 
-    beforeEach(inject(function (_$rootScope_, $templateCache) {
-        rootScope = _$rootScope_;
+    beforeEach(inject(function (_$rootScope_, _$q_, $templateCache) {
+        $rootScope = _$rootScope_;
+        $q = _$q_;
         templateCache = $templateCache;
     }));
 
-    it('[Boyko-Karadzhov] / should request items initially.', function () {
-        var scope = rootScope.$new();
-        var childrenRequested = false;
+    it('[Boyko-Karadzhov] / should request items initially once with null parent.', function () {
+        var scope = $rootScope.$new();
+        var childrenRequestedCount = 0;
+        var requestedParent = 'not null';
 
         templateCache.put('/Frontend-Assembly/Telerik.Sitefinity.Frontend/sf-tree/dummy.html', '');
 
-        scope.requestChildren = function () {
-            childrenRequested = true;
+        scope.requestChildren = function (parent) {
+            childrenRequestedCount++;
+            requestedParent = parent;
+
+            var result = $q.defer();
+            result.resolve({});
+            return result.$promise;
         };
         
         var directiveMarkup = '<div sf-tree sf-template-url="sf-tree/dummy.html" sf-request-children="requestChildren(parent)"></div>';
         commonMethods.compileDirective(directiveMarkup, scope);
 
-        expect(childrenRequested).toBe(true);
+        expect(childrenRequestedCount).toEqual(1);
+        expect(requestedParent).toEqual(null);
+    });
+
+    it('[Boyko-Karadzhov] / should request children for item when expanded.', function () {
+        var scope = $rootScope.$new();
+        var requestedParent = null;
+        var requestedWithParentCount = 0;
+
+        templateCache.put('/Frontend-Assembly/Telerik.Sitefinity.Frontend/sf-tree/expand-item.html', '<span ng-click="expand(node)">{{node.Id}}</span><ul><li ng-repeat="node in node.children" ng-include="sf-tree/expand-item.html"></li></ul>');
+        templateCache.put('/Frontend-Assembly/Telerik.Sitefinity.Frontend/sf-tree/expand.html', '<ul><li ng-repeat="node in hierarchy" ng-include="sf-tree/expand-item.html"></li></ul>');
+
+        scope.requestChildren = function (parent) {
+            var result = $q.defer();
+
+            if (!parent) {
+                result.resolve([{ Id: '1' }, { Id: '2' }]);
+            }
+            else {
+                result.resolve({});
+                requestedParent = parent;
+                requestedWithParentCount++;
+            }
+
+            return result.$promise;
+        };
+
+        var directiveMarkup = '<div sf-tree sf-template-url="sf-tree/expand.html" sf-request-children="requestChildren(parent)"></div>';
+        commonMethods.compileDirective(directiveMarkup, scope);
+
+        $('ul li span:contains("2")').click();
+        scope.$digest();
+
+        expect(requestedWithParentCount).toEqual(1);
+        expect(requestedParent).toEqual('2');
     });
 });
