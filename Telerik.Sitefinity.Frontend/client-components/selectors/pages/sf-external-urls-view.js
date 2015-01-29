cad224ae0c338a3aba6ae7e337b5df60cef486b8
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-01-29 18:49:17 +0200

    Removed method for generating of GUIDs
    Now we use the method that kendo provide.

--- Telerik.Sitefinity.Frontend/client-components/selectors/pages/sf-external-urls-view.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/pages/sf-external-urls-view.js
@@ -19,19 +19,6 @@
                 link: {
                     post: function (scope, element, attrs) {
 
-                        //creates new guid
-                        var guid = (function () {
-                            function s4() {
-                                return Math.floor((1 + Math.random()) * 0x10000)
-                                           .toString(16)
-                                           .substring(1);
-                            }
-                            return function () {
-                                return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-                                       s4() + '-' + s4() + s4() + s4();
-                            };
-                        })();
-
                         // The view is binded to this collection
                         if (!scope.sfExternalPages)
                             scope.sfExternalPages = [];
@@ -75,7 +62,7 @@
                         };
 
                         scope.addItem = function () {
-                            scope.sfExternalPagesInDialog.push({ ExternalPageId: guid(), TitlesPath: '', Url: '', Status: 'new' });
+                            scope.sfExternalPagesInDialog.push({ ExternalPageId: kendo.guid(), TitlesPath: '', Url: '', Status: 'new' });
 
                         };
 
