97100a8f38e37bfe9b72a346437c32c854be3106
Boyko Karadzhov <bokovskii@gmail.com>
2015-01-29 17:09:42 +0200

    added specs for sf-tree

---
+++ Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/collections/sfTreeSpec.js
@@ -0,0 +1,27 @@
+﻿describe('tree directive', function () {
+    beforeEach('sfTree');
+
+    var rootScope;
+    var templateCache;
+
+    beforeEach(inject(function (_$rootScope_, $templateCache) {
+        rootScope = _$rootScope_;
+        templateCache = $templateCache;
+    }));
+
+    it('[Boyko-Karadzhov] / should request items initially.', function () {
+        var scope = rootScope.$new();
+        var childrenRequested = false;
+
+        templateCache.put('/Frontend-Assembly/Telerik.Sitefinity.Frontend/sf-tree/dummy.html', '');
+
+        scope.requestChildren = function () {
+            childrenRequested = true;
+        };
+        
+        var directiveMarkup = '<div sf-tree sf-template-url="sf-tree/dummy.html"></div>';
+        commonMethods.compileDirective(directiveMarkup, scope);
+
+        expect(childrenRequested).toBe(true);
+    });
+});
