7c232748ddfcec92b1637bb41f29fd2477d47d0a
Boyko Karadzhov <bokovskii@gmail.com>
2015-01-30 11:30:28 +0200

    fixed sf-tree spec errors

--- Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/collections/sfTreeSpec.js
+++ Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/collections/sfTreeSpec.js
@@ -25,7 +25,7 @@
 
             var result = $q.defer();
             result.resolve({});
-            return result.$promise;
+            return result.promise;
         };
         
         var directiveMarkup = '<div sf-tree sf-template-url="sf-tree/dummy.html" sf-request-children="requestChildren(parent)"></div>';
@@ -40,7 +40,7 @@
         var requestedParent = null;
         var requestedWithParentCount = 0;
 
-        templateCache.put('/Frontend-Assembly/Telerik.Sitefinity.Frontend/sf-tree/expand-item.html', '<span ng-click="toggle(node)">{{node.Id}}</span><ul><li ng-repeat="node in node.children" ng-include="sf-tree/expand-item.html"></li></ul>');
+        templateCache.put('/Frontend-Assembly/Telerik.Sitefinity.Frontend/sf-tree/expand-item.html', '<span ng-click="toggle(node)">{{node.Id}}</span><ul><li ng-repeat="node in node.children" ng-include="sf-tree/expand-item.html" ng-class="{ \'sf-collapsed\': node.collapsed "></li></ul>');
         templateCache.put('/Frontend-Assembly/Telerik.Sitefinity.Frontend/sf-tree/expand.html', '<ul><li ng-repeat="node in hierarchy" ng-include="sf-tree/expand-item.html"></li></ul>');
 
         scope.requestChildren = function (parent) {
@@ -55,7 +55,7 @@
                 requestedWithParentCount++;
             }
 
-            return result.$promise;
+            return result.promise;
         };
 
         var directiveMarkup = '<div sf-tree sf-template-url="sf-tree/expand.html" sf-request-children="requestChildren(parent)"></div>';
@@ -67,4 +67,7 @@
         expect(requestedWithParentCount).toEqual(1);
         expect(requestedParent).toEqual('2');
     });
+
+    //it('[Boyko-Karadzhov] / should mark preselected items bound by ng-model as selected.', function () {
+    //});
 });
