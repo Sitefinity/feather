6484cf173dac8450c4b52063ca0147468454d955
Boyko Karadzhov <bokovskii@gmail.com>
2015-01-30 18:44:36 +0200

    fixed modularity issues

--- Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-image-selector.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-image-selector.js
@@ -2,7 +2,7 @@
     var sfSelectors = angular.module('sfSelectors');
     sfSelectors.requires.push('sfImageSelector');
 
-    angular.module('sfImageSelector', ['sfServices', 'sfInfiniteScroll', 'sfCollection'])
+    angular.module('sfImageSelector', ['sfServices', 'sfInfiniteScroll', 'sfCollection', 'sfMediaBasicFilters'])
         .directive('sfImageSelector', ['serverContext', 'sfMediaService', 'serviceHelper', function (serverContext, sfMediaService, serviceHelper) {
             var constants = {
                 initialLoadedItemsCount: 50,
