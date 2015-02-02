c44e46b7e87cd204b856b9c89b26b9a3963c5f48
dzhenko <dzhenko@gmail.com>
2015-02-02 17:13:29 +0200

    Added final specs for library filter.

--- Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/Selectors/libraryFilterSpec.js
+++ Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/Selectors/libraryFilterSpec.js
@@ -2,12 +2,18 @@
     var $rootScope;
     var mediaService;
     var templateCache;
+    var $q;
 
     var getFoldersObj = {
         getFolders: function (parent) {
-            return [
-                { Title: 'Title', Id: '1' }
+            var items = [
+                { Title: 'Title1', Id: '1' },
+                { Title: 'Title2', Id: '2' }
             ];
+
+            var defer = $q.defer();
+            defer.resolve({ Items: items });
+            return defer.promise;
         }
     };
 
@@ -18,13 +24,79 @@
         mediaService = $injector.get('sfMediaService');
     }));
 
-    beforeEach(inject(function (_$rootScope_, $templateCache) {
+    beforeEach(inject(function (_$rootScope_, _$q_, $templateCache) {
         $rootScope = _$rootScope_;
+        $q = _$q_;
         templateCache = $templateCache;
     }));
 
     beforeEach(function () {
         commonMethods.mockServerContextToEnableTemplateCache();
+    });
+
+    it('[dzhenko] / should properly set parent id of filter object', function () {
+        var scope = $rootScope.$new();
+
+        scope.filterObject = mediaService.newFilter();
+
+        mediaService.specTest = getFoldersObj;
+
+        var directiveMarkup = '<span sf-library-filter ng-model="filterObject" sf-media-type="specTest"></span>';
+
+        commonMethods.compileDirective(directiveMarkup, scope);
+
+        expect(scope.filterObject.parent).toBe(null);
+
+        $('ul li span:contains("Title1")').click();
+        scope.$digest();
+
+        expect(scope.filterObject.parent).toEqual('1');
+    });
+
+    it('[dzhenko] / should properly set basic property to null when parent Id is selected', function () {
+        var scope = $rootScope.$new();
+
+        scope.filterObject = mediaService.newFilter();
+
+        mediaService.specTest = getFoldersObj;
+
+        var directiveMarkup = '<span sf-library-filter ng-model="filterObject" sf-media-type="specTest"></span>';
+
+        commonMethods.compileDirective(directiveMarkup, scope);
+
+        scope.filterObject.basic = "Initial Value";
+        scope.$digest();
+
+        expect(scope.filterObject.parent).toBe(null);
+        expect(scope.filterObject.basic).toEqual("Initial Value");
+
+        $('ul li span:contains("Title1")').click();
+        scope.$digest();
+
+        expect(scope.filterObject.parent).toEqual('1');
+        expect(scope.filterObject.basic).toBe(null);
+    });
+
+    it('[dzhenko] / should properly set parent to null when clicked second time (deselected)', function () {
+        var scope = $rootScope.$new();
+
+        scope.filterObject = mediaService.newFilter();
+
+        mediaService.specTest = getFoldersObj;
+
+        var directiveMarkup = '<span sf-library-filter ng-model="filterObject" sf-media-type="specTest"></span>';
+
+        commonMethods.compileDirective(directiveMarkup, scope);
+
+        expect(scope.filterObject.parent).toBe(null);
+
+        $('ul li span:contains("Title1")').click();
+        scope.$digest();
+        expect(scope.filterObject.parent).toEqual('1');
+
+        $('ul li span:contains("Title1")').click();
+        scope.$digest();
+        expect(scope.filterObject.parent).toBe(null);
     });
 
     it('[dzhenko] / should properly throw exception if passed filter object is not of a kind sfMediaService.newFilter()', function () {
@@ -33,8 +105,6 @@
         scope.filterObject = { };
 
         mediaService.specTest = getFoldersObj;
-
-        templateCache.put('/Frontend-Assembly/Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-library-filter.html', '');
 
         var directiveMarkup = '<span sf-library-filter ng-model="filterObject" sf-media-type="specTest"></span>';
         
@@ -49,8 +119,6 @@
         scope.filterObject = mediaService.newFilter();
 
         mediaService.specTest = getFoldersObj;
-
-        templateCache.put('/Frontend-Assembly/Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-library-filter.html', '');
 
         var directiveMarkup = '<span sf-library-filter ng-model="filterObject" sf-media-type="unexistingType"></span>';
 
@@ -70,8 +138,6 @@
             }
         };
 
-        templateCache.put('/Frontend-Assembly/Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-library-filter.html', '');
-
         var directiveMarkup = '<span sf-library-filter ng-model="filterObject" sf-media-type="specTest"></span>';
 
         expect(function () {
@@ -85,8 +151,6 @@
         scope.filterObject = mediaService.newFilter();
 
         mediaService.specTest = getFoldersObj;
-
-        templateCache.put('/Frontend-Assembly/Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-library-filter.html', '');
 
         var directiveMarkup = '<span sf-library-filter ng-model="filterObject" sf-media-type="specTest"></span>';
 
