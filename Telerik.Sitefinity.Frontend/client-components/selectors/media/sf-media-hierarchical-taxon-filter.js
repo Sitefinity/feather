006f92a385bd49cd8e620a12ce9e14ece8c7a306
pivanova <penka.ivanova@telerik.com>
2015-02-03 12:07:08 +0200

    Added skeleton for sf-media-hierarchical-taxon-filter

---
+++ Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-media-hierarchical-taxon-filter.js
@@ -0,0 +1,31 @@
+﻿; (function ($) {
+    angular.module('sfMediaHierarchicalTaxonFilter', ['sfServices', 'sfTree'])
+        .directive('sfMediaHierarchicalTaxonFilter', ['serverContext', 'sfMediaService', 'sfHierarchicalTaxonService', function (serverContext, mediaService, taxonService) {
+            return {
+                restrict: 'AE',
+                scope: {
+                    filterObject: '=ngModel',
+                    sfField: '@',
+                    sfTitle: '@',
+                    sfTaxonomyId: '@'
+                },
+                templateUrl: function (elem, attrs) {
+                    var assembly = attrs.sfTemplateAssembly || 'Telerik.Sitefinity.Frontend';
+                    var url = attrs.sfTemplateUrl || 'client-components/selectors/media/sf-media-hierarchical-taxon-filter.html';
+                    return serverContext.getEmbeddedResourceUrl(assembly, url);
+                },
+                link: function (scope, element, attrs, ctrl) {
+
+                    scope.selectedItemId = null;
+
+                    scope.requestChildrenCallback = function (parent) {
+
+                    };
+
+                    scope.$watch('selectedItemId', function (newVal, oldVal) {
+
+                    });
+                }
+            };
+        }]);
+})(jQuery);
