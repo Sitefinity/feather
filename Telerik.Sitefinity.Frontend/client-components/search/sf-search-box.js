ea7bf0387f5a9f7c12b9a1e4a62332cba59413b6
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-02-03 18:04:25 +0200

    Added sf-search-box directive

---
+++ Telerik.Sitefinity.Frontend/client-components/search/sf-search-box.js
@@ -0,0 +1,33 @@
+﻿; (function ($) {
+    angular.module('sfSearchBox', [])
+        .directive('sfSearchBox', function () {
+            return {
+                restrict: 'AE',
+                scope: {
+                    sfAction: '@',
+                    sfMinTextLength: '@?',
+                    sfPlaceholder: '@?',
+                    sfEnableAutocomplete: '@',
+                    sfGetSuggestions: '@?'
+                },
+                templateUrl: function (elem, attrs) {
+                    var assembly = attrs.sfTemplateAssembly || 'Telerik.Sitefinity.Frontend';
+                    var url = attrs.sfTemplateUrl || 'client-components/search/sf-search-box.html';
+                    return serverContext.getEmbeddedResourceUrl(assembly, url);
+                },
+                link: function (scope, element, attrs, ctrl) {
+                    if (!scope.sfAction)
+                        scope.showError = true;
+
+                    if (!scope.sfMinTextLength)
+                        scope.sfMinTextLength = 3;
+
+                    scope.sfSearchCallback = function () {
+                        if (!scope.sfModel || scope.sfModel.length >= scope.sfMinTextLength) {
+                            scope.sfAction({ query: scope.sfModel });
+                        }
+                    };
+                }
+            };
+        });
+})(jQuery);
