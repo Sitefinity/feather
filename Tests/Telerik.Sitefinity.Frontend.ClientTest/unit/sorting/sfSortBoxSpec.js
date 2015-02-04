fb64cdf13571dcd2787b6a696292fbf4cabc93e9
dzhenko <dzhenko@gmail.com>
2015-02-04 15:34:31 +0200

    More sf-sort spec.

--- Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/sorting/sfSortBoxSpec.js
+++ Tests/Telerik.Sitefinity.Frontend.ClientTest/unit/sorting/sfSortBoxSpec.js
@@ -5,7 +5,11 @@
     var $rootScope;
     var $compile = null;
     var el = null;
-    var changeSelectedOption;
+
+    var changeSelectedOption = function (elm, value) {
+        elm.val(value);
+        browserTrigger(elm, 'change');
+    };
 
     beforeEach(inject(function (_$rootScope_) {
         $rootScope = _$rootScope_;
@@ -14,73 +18,33 @@
     beforeEach(function () {
         commonMethods.mockServerContextToEnableTemplateCache();
     });
-    
-    beforeEach(inject(function ($injector) {
-        changeSelectedOption = function () {
 
+    var generictTestForValue = function (sortOptionIndex, expectedValue) {
+        var scope = $rootScope.$new();
+        var directiveMarkup = '<div class="sort-box" sf-sort-box sf-action="callback"></div>';
+        commonMethods.compileDirective(directiveMarkup, scope);
+        var s = scope.$$childHead;
+
+        if (sortOptionIndex) {
+            s.sfModel = s.sfSortOptions[sortOptionIndex].value;
         }
-    }));
 
-    it('[dzhenko] / should have first sort value on default and not call callback function.', function () {
-        var scope = $rootScope.$new();
-        var setSortExpression = null;
-        var called = false;
+        expect(s.sfModel).toEqual(expectedValue);
+    };
 
-        scope.callback = function (sortExpression) {
-            setSortExpression = sortExpression;
-            called = true;
-        };
-
-        var directiveMarkup = '<div class="sort-box" sf-sort-box sf-action="callback"></div>';
-        var el = commonMethods.compileDirective(directiveMarkup, scope);
-        var s = scope.$$childHead;
-
-        //var ctrl = el.find('select').controller('ngModel');
-        //ctrl.$setViewValue('New-uploaded first');
-        //ctrl.$render();
-
-        //el.find('select').trigger('select');
-
-        $('.sort-box').change();
-
-        expect(s.sfModel).toEqual('DateCreated DESC');
-        expect(setSortExpression).toEqual(null);
-        expect(called).toEqual(false);
+    it('[dzhenko] / should have first sort value on default.', function () {
+        generictTestForValue(null, 'DateCreated DESC');
     });
 
-    it('[dzhenko] / should have second sort value when chosen and trigger callback.', function () {
-        var scope = $rootScope.$new();
-        var setSortExpression = null;
-        var called = false;
-
-        scope.callback = function (sortExpression) {
-            setSortExpression = sortExpression;
-            called = true;
-        };
-
-        var directiveMarkup = '<div class="sort-box" sf-sort-box sf-action="callback"></div>';
-        var el = commonMethods.compileDirective(directiveMarkup, scope);
-        var s = scope.$$childHead;
-
-        $('.sort-box select').val(s.sfSortOptions[1].value);
-
-        //var ctrl = el.find('select').controller('ngModel');
-        //ctrl.$setViewValue(s.sfSortOptions[1].value);
-        //ctrl.$render();
-        //s.$digest();
-
-        expect(s.sfModel).toEqual('LastModified DESC');
-        expect(setSortExpression).not.toEqual(null);
-        expect(called).toEqual(true);
+    it('[dzhenko] / should have proper second sort value.', function () {
+        generictTestForValue(1, 'LastModified DESC');
     });
 
-    //it('[dzhenko] / should throw error if no callback is passed to sf-action.', function () {
-    //    var scope = $rootScope.$new();
+    it('[dzhenko] / should have proper third sort value.', function () {
+        generictTestForValue(2, 'Title ASC');
+    });
 
-    //    var directiveMarkup = '<div sf-sort-box></div>';
-
-    //    expect(function () {
-    //        commonMethods.compileDirective(directiveMarkup, scope);
-    //    }).toThrow();
-    //});
+    it('[dzhenko] / should have proper third sort value.', function () {
+        generictTestForValue(3, 'Title DESC');
+    });
 });
