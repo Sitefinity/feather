b9d0ec849949c745737e38016f64ea6f86548115
dzhenko <dzhenko@gmail.com>
2015-02-05 17:44:56 +0200

    Initial refactoring.

--- Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-image-selector.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-image-selector.js
@@ -2,20 +2,55 @@
     var sfSelectors = angular.module('sfSelectors');
     sfSelectors.requires.push('sfImageSelector');
 
-    angular.module('sfImageSelector', ['sfServices', 'sfInfiniteScroll', 'sfCollection',
-                                        'sfMediaBasicFilters', 'sfLibraryFilter', 'sfMediaFlatTaxonFilter', 'sfMediaHierarchicalTaxonFilter', 'sfMediaDateFilter', 'sfSearchBox', 'sfSortBox'])
-        .directive('sfImageSelector', ['serverContext', 'sfMediaService', 'serviceHelper', function (serverContext, sfMediaService, serviceHelper) {
+    angular.module('sfImageSelector', ['sfServices', 'sfInfiniteScroll', 'sfCollection', 'sfTree', 'sfSearchBox', 'sfSortBox'])
+        .directive('sfImageSelector', ['serverContext', 'sfMediaService', 'serviceHelper', 'sfFlatTaxonService', 'sfHierarchicalTaxonService',
+        function (serverContext, sfMediaService, serviceHelper, sfFlatTaxonService, sfHierarchicalTaxonService) {
+            var helpers = {
+                getDate: function (daysToSubstract, monthsToSubstract, yearsToSubstract) {
+                    var now = new Date();
+
+                    if (daysToSubstract) {
+                        now.setDate(now.getDate() - daysToSubstract);
+                    }
+
+                    if (monthsToSubstract) {
+                        now.setMonth(now.getMonth() - monthsToSubstract);
+                    }
+
+                    if (yearsToSubstract) {
+                        now.setYear(now.getFullYear() - yearsToSubstract);
+                    }
+
+                    return now;
+                }
+            };
+
             var constants = {
                 initialLoadedItemsCount: 50,
                 infiniteScrollLoadedItemsCount: 20,
                 recentImagesLastDaysCount: 7,
-                filterOptions: {
-                    basic: {
-                        allLibraries: 'AllLibraries',
-                        ownItems: 'OwnItems',
-                        recentItems: 'RecentItems'
+                filters: {
+                    dates: [
+                        { text: 'Any time', dateValue: null },
+                        { text: 'Last 1 day', dateValue: helpers.getDate(1) },
+                        { text: 'Last 3 days', dateValue: helpers.getDate(3) },
+                        { text: 'Last 1 week', dateValue: helpers.getDate(7) },
+                        { text: 'Last 1 month', dateValue: helpers.getDate(0, 1) },
+                        { text: 'Last 6 months', dateValue: helpers.getDate(0, 6) },
+                        { text: 'Last 1 year', dateValue: helpers.getDate(0, 0, 1) },
+                        { text: 'Last 2 years', dateValue: helpers.getDate(0, 0, 2) },
+                        { text: 'Last 5 years', dateValue: helpers.getDate(0, 0, 5) }
+                    ],
+                    tags: {
+                        pageSize: 10,
+                        field: 'Tags',
+                        taxonomyId: 'CB0F3A19-A211-48a7-88EC-77495C0F5374'
                     },
-                    dateCreatedDescending: 'DateCreated DESC'
+                    categories: {
+                        pageSize: 10, // not used by the service
+                        field: 'Category',
+                        taxonomyId: 'E5CD6D69-1543-427B-AD62-688A99F5E7D4'
+                    }
                 }
             };
 
@@ -30,64 +65,72 @@
                     return serverContext.getEmbeddedResourceUrl(assembly, url);
                 },
                 link: function (scope, element, attrs, ctrl) {
-                    scope.sortExpression = null;
-                    scope.items = [];
-                    scope.filterObject = sfMediaService.newFilter();
-                    scope.isLoading = false;
-                    scope.showSortingAndView = false;
-
-                    scope.loadMore = function () {
-                        refresh(true);
-                    };
-
-                    scope.narrowResults = function (query) {
-                        if (query && scope.filterObject.basic === constants.filterOptions.basic.allLibraries) {
-                            scope.filterObject.basic = null;
-                        }
-
-                        scope.filterObject.query = query || null;
-                    };
-
-                    scope.$watch('filterObject', function (newVal, oldVal) {
-                        if (newVal) {
-                            var newValSerialized = JSON.stringify(newVal);
-                            if (newValSerialized === JSON.stringify(sfMediaService.newFilter())) {
-                                scope.filterObject.basic = constants.filterOptions.basic.allLibraries;
+                    var filtersLogic = {
+                        // Library filter
+                        loadLibraryChildren: function (parent) {
+                            parent = parent || {};
+                            return sfMediaService.images.getFolders({ parent: parent.Id }).then(function (response) {
+                                if (response) {
+                                    return response.Items;
+                                }
+                            });
+                        },
+                        // Category filter
+                        getCategoryChildTaxons: function (parentId) {
+                            return sfHierarchicalTaxonService.getChildTaxons(parentId, scope.query)
+                                .then(function (data) {
+                                    return data.Items;
+                                });
+                        },
+                        getCategoryTaxons: function () {
+                            var skip = 0;
+                            return sfHierarchicalTaxonService.getTaxons(constants.filters.categories.taxonomyId, skip, constants.filters.categories.pageSize, scope.filters.category.query)
+                                .then(function (data) {
+                                    return data.Items;
+                                });
+                        },
+                        loadCategoryChildren: function (parent) {
+                            if (parent) {
+                                return filtersLogic.getCategoryChildTaxons(parent.Id);
+                            }
+                            else {
+                                return filtersLogic.getCategoryTaxons();
+                            }
+                        },
+                        // Tag filter
+                        loadTagTaxons: function (append) {
+                            if (scope.filters.tag.isLoading) {
                                 return;
                             }
 
-                            if (newValSerialized !== JSON.stringify(oldVal)) {
-                                if (newVal.basic === constants.filterOptions.basic.recentItems && scope.sortExpression !== constants.filterOptions.dateCreatedDescending) {
-                                    scope.sortExpression = constants.filterOptions.dateCreatedDescending;
-                                }
-                                else {
-                                    refresh();
-                                }
-                            }
+                            scope.filters.tag.isLoading = true;
+                            var skip = append ? scope.filters.tag.all.length : 0;
+                            sfFlatTaxonService.getTaxons(constants.filters.tags.taxonomyId, skip, constants.filters.tags.pageSize, scope.filters.tag.query)
+                                .then(function (data) {
+                                    if (data && data.Items) {
+                                        if (append) {
+                                            if (scope.filters.tag.all && scope.filters.tag.all.length === skip) {
+                                                scope.filters.tag.all = scope.filters.tag.all.concat(data.Items);
+                                            }
+                                        }
+                                        else {
+                                            scope.filters.tag.all = data.Items;
+                                        }
+                                    }
+                                })
+                                .finally(function () {
+                                    scope.filters.tag.isLoading = false;
+                                });
+                        },
+                        loadMoreTags: function () {
+                            filtersLogic.loadTagTaxons(true);
                         }
-                    }, true);
-
-                    scope.$watch('sortExpression', function (newVal, oldVal) {
-                        if (newVal !== oldVal) {
-                            if (newVal !== constants.filterOptions.dateCreatedDescending && scope.filterObject.basic === constants.filterOptions.basic.recentItems) {
-                                scope.filterObject.basic = null;
-                            }
-                            else {
-                                refresh();
-                            }
-                        }
-                    });
-
-                    scope.$on('sf-collection-item-selected', function (event, data) {
-                        if (data && data.IsFolder === true) {
-                            scope.filterObject.basic = null;
-                            scope.filterObject.parent = data.Id;
-                        }
-                    });
+                    };
 
                     var refresh = function (appendItems) {
-                        if (scope.isLoading)
+                        if (scope.isLoading) {
                             return;
+                        }
 
                         var options = {
                             filter: scope.filterObject.composeExpression()
@@ -110,13 +153,16 @@
                         }
                         else if (scope.filterObject.basic) {
                             // Defaul filter is used (Recent / My / All)
-                            if (scope.filterObject.basic === constants.filterOptions.basic.recentItems) {
+                            if (scope.filterObject.basic === scope.filterObject.constants.basic.recentItems) {
+                                // When the filter is Recent items, the number of displayed items is fixed and we should not append more.
+                                if (appendItems) return;
+
                                 callback = sfMediaService.images.getImages;
                             }
-                            else if (scope.filterObject.basic === constants.filterOptions.basic.ownItems) {
+                            else if (scope.filterObject.basic === scope.filterObject.constants.basic.ownItems) {
                                 callback = sfMediaService.images.getImages;
                             }
-                            else if (scope.filterObject.basic === constants.filterOptions.basic.allLibraries) {
+                            else if (scope.filterObject.basic === scope.filterObject.constants.basic.allLibraries) {
                                 callback = sfMediaService.images.getFolders;
                             }
                             else {
@@ -151,12 +197,133 @@
                         }
                     };
 
-                    // initial open populates dialog with all root libraries
-                    scope.filterObject.basic = constants.filterOptions.basic.allLibraries;
-                    refresh();
+                    scope.sortExpression = null;
+                    scope.items = [];
+                    scope.filterObject = sfMediaService.newFilter();
+                    scope.isLoading = false;
+                    scope.showSortingAndView = false;
 
-                    // initial filter dropdown option
-                    scope.selectedFilterOption = 1;
+                    scope.filters = {
+                        library: {
+                            selected: null,
+                            getChildren: filtersLogic.loadLibraryChildren
+                        },
+                        date: {
+                            all: constants.filters.dates,
+                            selected: constants.filters.dates[0].dateValue
+                        },
+                        tag: {
+                            all: [],
+                            selected: null,
+                            query: null,
+                            isLoading: false,
+                            loadMore: filtersLogic.loadMoreTags
+                        },
+                        category: {
+                            filtered: [],
+                            selected: null,
+                            query: null,
+                            getChildren: filtersLogic.loadCategoryChildren
+                        }
+                    };
+
+                    scope.narrowResults = scope.filterObject.set.query.to;
+
+                    // load more images
+                    scope.loadMore = function () {
+                        refresh(true);
+                    };
+
+                    /*
+                    * Watches.
+                    */
+
+                    scope.$watch('sortExpression', function (newVal, oldVal) {
+                        if (newVal !== oldVal) {
+                            if (newVal !== scope.filterObject.constants.dateCreatedDescending && scope.filterObject.basic === scope.filterObject.constants.basic.recentItems) {
+                                scope.filterObjects.set.basic.none();
+                            }
+                            else {
+                                refresh();
+                            }
+                        }
+                    });
+
+                    scope.$watch('filters.library.selected', function (newVal, oldVal) {
+                        if (newVal && newVal !== oldVal) {
+                            scope.filterObject.set.parent.to(newVal);
+                        }
+                    }, true);
+
+                    scope.$watch('filters.tag.selected', function (newVal, oldVal) {
+                        if (newVal && newVal !== oldVal) {
+                            if (newVal && newVal[0]) {
+                                scope.filterObject.set.taxon.to(newVal[0], constants.filters.tags.field);
+                            }
+                        }
+                    }, true);
+
+                    scope.$watch('filters.tag.query', function (newVal, oldVal) {
+                        if (newVal !== oldVal) {
+                            filtersLogic.loadTagTaxons(false);
+                        }
+                    });
+
+                    scope.$watch('filters.category.selected', function (newVal, oldVal) {
+                        if (newVal && newVal !== oldVal) {
+                            var selectedTaxonId;
+                            if (angular.isArray(newVal) && newVal.length > 0) {
+                                selectedTaxonId = newVal[0];
+                            }
+                            else {
+                                selectedTaxonId = newVal;
+                            }
+                            
+                            scope.filterObject.set.taxon.to(selectedTaxonId, constants.filters.categories.field);
+                        }
+                    }, true);
+
+                    scope.$watch('filters.category.query', function (newVal, oldVal) {
+                        if (newVal !== oldVal) {
+                            filtersLogic.getCategoryTaxons().then(function (items) {
+                                scope.filters.category.filtered = items;
+                            });
+                        }
+                    });
+
+                    scope.$watch('filters.date.selected', function (newVal, oldVal) {
+                        if (newVal !== oldVal) {
+                            if (newVal && newVal[0]) {
+                                scope.filterObject.set.date.to(newVal[0]);
+                            }
+                            else {
+                                scope.filterObject.set.date.all();
+                            }
+                        }
+                    }, true);
+
+                    // Reacts when a folder is clicked.
+                    scope.$on('sf-collection-item-selected', function (event, data) {
+                        if (data && data.IsFolder === true) {
+                            scope.filterObject.set.parent.to(data.Id)
+                        }
+                    });
+
+                    /*
+                    * Initialization.
+                    */
+
+                    (function initializeWindow() {
+                        // initial open populates dialog with all root libraries
+                        scope.filterObject.set.basic.allLibraries();
+
+                        // initial filter dropdown option
+                        scope.selectedFilterOption = 1;
+
+                        filtersLogic.loadTagTaxons(false);
+
+                        scope.filterObject.attachEvent(refresh);
+                    }());
                 }
             };
         }]);
--- Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-media-service.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-media-service.js
@@ -2,8 +2,8 @@
     angular.module('sfServices').factory('sfMediaService', ['serviceHelper', 'serverContext', function (serviceHelper, serverContext) {
         var constants = {
             images: {
-                itemType : 'Telerik.Sitefinity.Libraries.Model.Image',
-                albumsServiceUrl : serverContext.getRootedUrl('Sitefinity/Services/Content/AlbumService.svc/folders/'),
+                itemType: 'Telerik.Sitefinity.Libraries.Model.Image',
+                albumsServiceUrl: serverContext.getRootedUrl('Sitefinity/Services/Content/AlbumService.svc/folders/'),
                 imagesServiceUrl: serverContext.getRootedUrl('Sitefinity/Services/Content/ImageService.svc/')
             }
         };
@@ -18,6 +18,19 @@
         };
 
         var MediaFilter = function () {
+            var self = this;
+            var events = [];
+
+            this.constants = {
+                basic: {
+                    allLibraries: 'AllLibraries',
+                    ownItems: 'OwnItems',
+                    recentItems: 'RecentItems'
+                },
+                dateCreatedDescending: 'DateCreated DESC',
+                anyTimeValue: 'AnyTime'
+            };
+
             // Query that is typed by a user in a text box.
             this.query = null;
 
@@ -33,10 +46,16 @@
             // Filter by any taxon
             this.taxon = new TaxonFilter();
 
+            this.attachEvent = function (callback) {
+                if (typeof callback === 'function') {
+                    events.push(callback);
+                }
+            };
+
             this.composeExpression = function () {
                 var expression = serviceHelper.filterBuilder();
 
-                if (this.basic !== 'AllLibraries') {
+                if (this.basic !== this.constants.basic.allLibraries) {
                     expression = expression.lifecycleFilter();
                 }
 
@@ -44,10 +63,10 @@
                     expression = expression.and().searchFilter(this.query);
                 }
 
-                if (this.basic && this.basic === 'OwnItems')
+                if (this.basic === this.constants.basic.ownItems)
                     expression = expression.and().append('Owner == (' + serverContext.getCurrentUserId() + ')');
 
-                if (this.date && this.date !== 'AnyTime') {
+                if (this.date && this.date !== this.constants.anyTimeValue) {
                     expression = expression.and().append('LastModified > (' + this.date.toGMTString() + ')');
                 }
 
@@ -55,6 +74,98 @@
                     expression = expression.and().append(this.taxon.composeExpression());
 
                 return expression.getFilter();
+            };
+
+            this.set = {
+                basic: {
+                    none: function () {
+                        if (self.basic) {
+                            self.basic = null;
+                            changed();
+                        }
+                    },
+                    allLibraries: function () {
+                        if (self.basic !== self.constants.basic.allLibraries) {
+                            reset();
+                            self.basic = self.constants.basic.allLibraries;
+                            changed();
+                        }
+                    },
+                    ownItems: function () {
+                        if (self.basic !== self.constants.basic.ownItems) {
+                            reset();
+                            self.basic = self.constants.basic.ownItems;
+                            changed();
+                        }
+                    },
+                    recentItems: function () {
+                        if (self.basic !== self.constants.basic.recentItems) {
+                            reset();
+                            self.basic = self.constants.basic.recentItems;
+                            changed();
+                        }
+                    }
+                },
+                date: {
+                    all: function () {
+                        if (self.date !== self.constants.anyTimeValue) {
+                            reset();
+                            self.date = self.constants.anyTimeValue;
+                            changed();
+                        }
+                    },
+                    to: function (date) {
+                        if (self.date !== date) {
+                            reset();
+                            self.date = date;
+                            changed();
+                        }
+                    }
+                },
+                parent: {
+                    to: function (parentId) {
+                        if (self.parent !== parentId) {
+                            reset();
+                            self.parent = parentId;
+                            changed();
+                        }
+                    }
+                },
+                taxon: {
+                    to: function (id, field) {
+                        if (!self.taxon || self.taxon.id !== id || self.taxon.field !== field) {
+                            reset();
+                            self.taxon.id = id;
+                            self.taxon.field = field;
+                            changed();
+                        }
+                    }
+                },
+                query: {
+                    to: function (query) {
+                        if (query && self.basic === self.constants.basic.allLibraries) {
+                            self.basic = null;
+                        }
+
+                        self.query = query;
+                        changed();
+                    }
+                }
+
+            }
+
+            var reset = function () {
+                self.query = null;
+                self.basic = null;
+                self.parent = null;
+                self.date = null;
+                self.taxon = new TaxonFilter();
+            };
+
+            var changed = function () {
+                events.forEach(function (callback) {
+                    callback();
+                });
             };
         };
 
