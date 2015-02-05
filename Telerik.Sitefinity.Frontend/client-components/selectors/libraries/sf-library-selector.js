8a07ca853bc05606c2a90499b98ae12dfffa9f12
NadezhdaPetrova <nadezhda.petrova@hotmail.com>
2015-02-05 17:22:38 +0200

    Added getSpecificItems logic and logic about generating breadcrumbs

--- Telerik.Sitefinity.Frontend/client-components/selectors/libraries/sf-library-selector.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/libraries/sf-library-selector.js
@@ -1,6 +1,35 @@
 ﻿(function ($) {
     angular.module('sfSelectors')
         .directive('sfLibrarySelector', ['serviceHelper', 'sfMediaService', function (serviceHelper, mediaService) {
+            var _applyBreadcrumbPath = function (result) {
+                //var taxa = result.Items;
+                //var taxonToAdd = null;
+                //var taxonPathTitle = '';
+                //var taxaLength = taxa.length;
+                //if (taxaLength > 0) {
+                //    var taxonId = taxa[taxaLength - 1].Id;
+                //    var delimiter = ' > ';
+                //    for (var i = 0, l = taxa.length; i < l; i++) {
+                //        if (i == taxa.length - 1) {
+                //            delimiter = '';
+                //            taxonToAdd = taxa[i];
+                //        }
+                //        taxonPathTitle += taxa[i].Title + delimiter;
+                //    }
+                //    if (taxonId !== taxonToAdd.Id) {
+                //        throw 'unexpected end of the taxon path.';
+                //    }
+                //    taxonToAdd.TitlesPath = taxonToAdd.TitlesPath || taxonPathTitle;
+
+                //    taxonToAdd.Breadcrumb = taxonToAdd.TitlesPath;
+                //}
+                //else {
+                //    throw "Getting the taxon path returned an empty collection.";
+                //}
+                //return taxonToAdd;
+                return null;
+            };
+
             return {
                 require: '^sfListSelector',
                 restrict: 'A',
@@ -8,20 +37,16 @@
                     pre: function (scope, element, attrs, ctrl) {
                         var mediaType = attrs.sfMediaType;
 
-                        var getFilter = function (search) {
+                        ctrl.getItems = function (skip, take, search) {
                             var filter = serviceHelper.filterBuilder()
                                                       .searchFilter(search)
                                                       .getFilter();
 
-                            return filter;
-                        };
-
-                        ctrl.getItems = function (skip, take, search) {
                             var options = {
                                 parent: null,
                                 skip: skip,
                                 take: take,
-                                filter: getFilter(search),
+                                filter: filter,
                                 recursive: search ? true : null,
                                 sort: "Title ASC"
                             };
@@ -35,9 +60,13 @@
                         };
 
                         ctrl.getChildren = function (parentId, search) {
+                            var filter = serviceHelper.filterBuilder()
+                                                      .searchFilter(search)
+                                                      .getFilter();
+
                             var options = {
                                 parent: parentId,
-                                filter: getFilter(search),
+                                filter: filter,
                                 sort: "Title ASC"
                             };
 
@@ -50,8 +79,42 @@
                         };
 
                         ctrl.getSpecificItems = function (ids) {
-                            //return hierarchicalTaxonService.getSpecificItems(taxonomyId, ids);
-                            return null;
+                            var filter = serviceHelper.filterBuilder()
+                                                      .specificItemsFilter(ids)
+                                                      .getFilter();
+
+                            var options = {
+                                skip: 0,
+                                take: 100,
+                                filter: filter,
+                            };
+
+                            switch (mediaType) {
+                                case 'image':
+                                    return mediaService.images.getFolders(options);
+                                default:
+                                    return mediaService.images.getFolders(options);
+                            }
+                        };
+
+                        ctrl.onSelectedItemsLoadedSuccess = function (data) {
+                            var items = [];
+
+                            angular.forEach(data.Items, function (result) {
+                                items.push(_applyBreadcrumbPath({ Items: result }));
+                            });
+
+                            ctrl.updateSelection(items);
+                        };
+
+                        ctrl.onItemSelected = function (item) {
+                            item.Breadcrumb = item.TitlesPath ? item.TitlesPath + " > " + item.Title : item.Title;
+                        };
+
+                        ctrl.onFilterItemSucceeded = function (items) {
+                            angular.forEach(items, function (item) {
+                                item.RootPath = item.TitlesPath ? "Under " + item.TitlesPath : 'On Top Level';
+                            });
                         };
 
                         ctrl.selectorType = 'LibrarySelector';
@@ -61,6 +124,8 @@
                             'client-components/selectors/common/sf-bubbles-selection.html';
 
                         ctrl.$scope.hierarchical = true;
+                        ctrl.$scope.sfIdentifierField = "Breadcrumb";
+                        ctrl.$scope.searchIdentifierField = "Title";
                     }
                 }
             };
