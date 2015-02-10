351db9bfef0507d481ea240b49561683c49d646b
Boyko Karadzhov <bokovskii@gmail.com>
2015-02-10 11:46:47 +0200

    media markup service initial commit

---
+++ Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-media-markup.js
@@ -0,0 +1,18 @@
+﻿; (function () {
+    angular.module('sfMediaMarkupService', ['sfServices'])
+        .factory('sfMediaMarkupService', [function () {
+
+            var image = {
+                markup: function (properties) {
+
+                },
+                properties: function (markup) {
+
+                }
+            };
+
+            return {
+                image: image
+            };
+        }]);
+})();
