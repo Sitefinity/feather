e62b108cb77b0bf57e1494f8812af2bccf0b429b
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-02-11 17:44:55 +0200

    Enabled search in subfolders

--- Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-media-service.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-media-service.js
@@ -25,6 +25,7 @@
 
         var getItems = function (options, excludeFolders, serviceUrl, itemType) {
             options = options || {};
+            excludeFolders = excludeFolders ? excludeFolders : options.excludeFolders;
 
             var url = options.parent ? serviceUrl + 'parent/' + options.parent + "/" : serviceUrl;
 
@@ -36,7 +37,7 @@
                     skip: options.skip,
                     take: options.take,
                     sortExpression: options.sort,
-                    includeSubFolderItems: options.recursive ? 'true' : null,
+                    includeSubFoldersItems: options.recursive ? 'true' : null,
                     excludeFolders: excludeFolders
                 }).$promise;
         };
@@ -201,6 +202,21 @@
                     .then(function (settings) {
                         var allLanguageSearch = settings.EnableAllLanguagesSearch.toLowerCase() === 'true';
                         options.filter = filterObject.composeExpression(allLanguageSearch);
+
+                        var selectedFolderSearch = settings.EnableSelectedFolderSearch.toLowerCase() === 'true';
+                        if (filterObject.query) {
+                            if (selectedFolderSearch) {
+                                options.parent = filterObject.parent;
+                                options.recursive = true;
+                                options.excludeFolders = true;
+                            }
+                            else {
+                                options.parent = null;
+                                options.recursive = false;
+                                options.excludeFolders = false;
+                            }
+                        }
+
                         return callback(options);
                     });
             },
