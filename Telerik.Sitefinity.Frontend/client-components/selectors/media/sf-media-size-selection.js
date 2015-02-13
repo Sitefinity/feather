14c0adfefe1ea5bb05415935498787fefcd8e1d0
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-02-13 15:27:57 +0200

    added opening for custom thumbnail size dialog

--- Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-media-size-selection.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/media/sf-media-size-selection.js
@@ -5,10 +5,11 @@
         custom: 'Custom'
     };
 
-    angular.module('sfMediaSizeSelection', [])
-        .controller('sfMediaSizeSelectionCtrl', ['$scope', function ($scope) {
+    angular.module('sfMediaSizeSelection', ['sfServices'])
+        .controller('sfMediaSizeSelectionCtrl', ['$scope', 'serverContext', function ($scope, serverContext) {
             $scope.sizeSelection = null;
             $scope.sizeOptions = [];
+            $scope.customThumbnailSizeTemplateUrl = serverContext.getEmbeddedResourceUrl('Telerik.Sitefinity.Frontend', 'client-components/selectors/media/sf-custom-thumbnail-size.html');
 
             $scope.$watch('sizeSelection', function (newVal, oldVal) {
                 if (!newVal || !$scope.model)
@@ -17,6 +18,28 @@
                 $scope.model.displayMode = newVal.type;
                 $scope.model.thumbnail = newVal.thumbnail;
                 $scope.model.customSize = newVal.customSize;
+                openDialog();
             });
+
+            var openDialog = function (file) {
+
+                angular.element('.thumbnailSizeModal').scope().$openModalDialog({ model: function () { return scope.model; } });
+            };
+        }])
+        .controller('sfCustomThumbnailSizeCtrl', ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+            $scope.constants = {
+                generationMethod: {
+                    resize: 'ResizeFitToAreaArguments',
+                    crop: 'CropCropArguments'
+                },
+                quality: ['High', 'Medium', 'Low']
+            }
+
+            $scope.generateThumbnail = function () {
+            };
+
+            $scope.cancelResizing = function () {
+                $modalInstance.close();
+            };
         }]);
 })();
