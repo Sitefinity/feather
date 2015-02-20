a40caebeadeb15e5b46de902fcbca1f408304b53
Boyko Karadzhov <bokovskii@gmail.com>
2015-02-20 14:20:09 +0200

    personalisation checko consistent with Sf

--- Telerik.Sitefinity.Frontend/Mvc/Infrastructure/Layouts/MasterPageBuilder.cs
+++ Telerik.Sitefinity.Frontend/Mvc/Infrastructure/Layouts/MasterPageBuilder.cs
@@ -282,12 +282,16 @@ namespace Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Layouts
 
             sb.Append(string.Concat("\t<script type=\"text/javascript\">var sf_appPath='", appPath, "';</script>"));
             
-            if (!SystemManager.IsDesignMode && Config.Get<SystemConfig>().ApplicationModules["Personalization"].StartupType != StartupType.Disabled)
+            if (!SystemManager.IsDesignMode)
             {
-                // add the scripts for personalization in the page
-                sb.Append("\t<script src=\"");
-                sb.Append(pageProxy.ClientScript.GetWebResourceUrl(typeof(PageStatisticsJSClient), "Telerik.Sitefinity.Clients.JS.StatsClient.min.js"));
-                sb.Append("\" type=\"text/javascript\"></script>");
+                IModule module = SystemManager.GetModule("Personalization");
+                if (module != null && SystemManager.CurrentContext.CurrentSite.IsModuleAccessible(module))
+                {
+                    // add the scripts for personalization in the page
+                    sb.Append("\t<script src=\"");
+                    sb.Append(pageProxy.ClientScript.GetWebResourceUrl(typeof(PageStatisticsJSClient), "Telerik.Sitefinity.Clients.JS.StatsClient.min.js"));
+                    sb.Append("\" type=\"text/javascript\"></script>");
+                }
             }
 
             return sb.ToString();
