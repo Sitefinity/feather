d997bd2b6eee744ef156310d179cd2605a3bee5e
NadezhdaPetrova <nadezhda.petrova@hotmail.com>
2015-03-17 11:08:29 +0200

    Added empty sf-document-selector and sf-media-selector

---
+++ Telerik.Sitefinity.Frontend/client-components/selectors/media/document/sf-document-selector.js
@@ -0,0 +1,9 @@
+﻿; (function ($) {
+    var sfSelectors = angular.module('sfSelectors');
+    sfSelectors.requires.push('sfDocumentSelector');
+
+    var sfDocumentSelector = angular.module('sfDocumentSelector', ['sfMediaSelector']);
+    sfDocumentSelector.directive('sfDocumentSelector', ['sfMediaSelector', function (sfMediaSelector) {
+
+    }]);
+})(jQuery);
