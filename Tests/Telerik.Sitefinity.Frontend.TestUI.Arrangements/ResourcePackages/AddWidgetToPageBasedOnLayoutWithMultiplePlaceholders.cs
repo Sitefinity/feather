df9c8c2c2bfb9ecc2938846241f80c0c90087e9a
PetyaRachina <petya.rachina@telerik.com>
2015-04-07 10:10:43 +0300

    added new UI test for multiple placeholders in layout template file

---
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/ResourcePackages/AddWidgetToPageBasedOnLayoutWithMultiplePlaceholders.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Telerik.Sitefinity.Frontend.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.Modules.Pages;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework.Attributes;
+using Telerik.Sitefinity.TestUtilities.CommonOperations;
+
+namespace Telerik.Sitefinity.Frontend.TestUI.Arrangements
+{
+    public class AddWidgetToPageBasedOnLayoutWithMultiplePlaceholders : ITestArrangement
+    {
+        [ServerArrangement]
+        public void AddNewLayoutFile()
+        {
+            this.CreateLayoutFolderAndCopyLayoutFile();
+        }
+
+        [ServerTearDown]
+        public void TearDown()
+        {
+            ServerOperations.Pages().DeleteAllPages();
+            ServerOperations.Templates().DeletePageTemplate(TemplateTitle);
+            string filePath = this.GetFilePath();
+            File.Delete(filePath);
+        }
+
+        private void CreateLayoutFolderAndCopyLayoutFile()
+        {
+            PageManager pageManager = PageManager.GetManager();
+            int templatesCount = pageManager.GetTemplates().Count();
+
+            string filePath = this.GetFilePath();
+
+            FeatherServerOperations.ResourcePackages().AddNewResource(FileResource, filePath);
+            FeatherServerOperations.ResourcePackages().WaitForTemplatesCountToIncrease(templatesCount, 1);
+        }
+
+        private string GetFilePath()
+        {
+            string folderPath = Path.Combine(FeatherServerOperations.ResourcePackages().SfPath, "MVC", "Views", "Layouts");
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            string filePath = Path.Combine(folderPath, LayoutFileName);
+
+            return filePath;
+        }
+
+        private const string FileResource = "Telerik.Sitefinity.Frontend.TestUtilities.Data.TestLayoutTwoPlaceholders.cshtml";
+        private const string LayoutFileName = "TestLayoutTwoPlaceholders.cshtml";
+        private const string TemplateTitle = "TestLayoutTwoPlaceholders";
+    }
+}
---
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.TestCases/ResourcePackages/LayoutTemplateWithMultiplePlaceholders.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Sitefinity.Frontend.TestUtilities;
+
+namespace Telerik.Sitefinity.Frontend.TestUI.TestCases.ResourcePackages
+{
+    /// <summary>
+    /// This is a test class with tests related to layout templates with multiple placeholders
+    /// </summary>
+    [TestClass]
+    public class LayoutTemplateWithMultiplePlaceholders : FeatherTestCase
+    {
+        // <summary>
+        /// UI test AddWidgetToPageBasedOnLayoutWithMultiplePlaceholders.
+        /// </summary>
+        [TestMethod,
+        Owner(FeatherTeams.Team2),
+        TestCategory(FeatherTestCategories.PagesAndContent)]
+        public void AddWidgetToPageBasedOnLayoutWithMultiplePlaceholders()
+        {
+            BAT.Macros().NavigateTo().Pages();
+            BAT.Wrappers()
+            
+        }
+
+        /// <summary>
+        /// Performs Server Setup and prepare the system with needed data.
+        /// </summary>
+        protected override void ServerSetup()
+        {
+            BAT.Arrange(this.TestName).ExecuteSetUp();
+        }
+
+        /// <summary>
+        /// Performs clean up and clears all data created by the test.
+        /// </summary>
+        protected override void ServerCleanup()
+        {
+            BAT.Arrange(this.TestName).ExecuteTearDown();
+        }
+
+        private const string PageTitle = "FeatherPage";
+    }
+}
