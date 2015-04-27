714d893c23e1e159030de2bb98df8577e03fd7ad
dzhenko <dzhenko@gmail.com>
2015-04-27 14:51:44 +0300

    Attached style inline loading to route handler load

--- Telerik.Sitefinity.Frontend/Mvc/Infrastructure/Routing/MvcPageEditorRouteHandler.cs
+++ Telerik.Sitefinity.Frontend/Mvc/Infrastructure/Routing/MvcPageEditorRouteHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Routing;
+using System.Web.UI;
 using Telerik.Sitefinity.Pages.Model;
 using Telerik.Sitefinity.Web;
 
@@ -21,7 +22,23 @@ namespace Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Routing
             var handler = base.BuildHandler(requestContext, pageData);
             new PageInitializer().Initialize(handler);
 
+            var page = handler as Page;
+            if (page != null)
+            {
+                page.Init += this.MvcPageEditorRouteHandlerInit;
+            }
+
             return handler;
+        }
+
+        private void MvcPageEditorRouteHandlerInit(object sender, System.EventArgs e)
+        {
+            var page = (Page)sender;
+
+            var inlineCss = @".sfMvcWidget { color : blue!important; }";
+            var literalText = string.Format(@"<style type=""text/css"">{0}</style>", inlineCss);
+
+            page.Header.Controls.Add(new LiteralControl(literalText));
         }
     }
 }
