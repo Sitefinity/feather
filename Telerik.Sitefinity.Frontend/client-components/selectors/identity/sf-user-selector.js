7b8dc8a70430b7f4d460659621149f258ea81a9b
NadezhdaPetrova <nadezhda.petrova@hotmail.com>
2015-04-28 17:51:05 +0300

    Added user providers dropdown to sf-user-selector

--- Telerik.Sitefinity.Frontend/client-components/selectors/identity/sf-user-selector.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/identity/sf-user-selector.js
@@ -6,34 +6,124 @@
                 restrict: 'A',
                 link: {
                     pre: function (scope, element, attrs, ctrl) {
-                        ctrl.getItems = function (skip, take, search, frontendLanguages) {
-                            var provider = ctrl.$scope.sfProvider;
-                            var ignoreAdminUsers = false;
-                            var allProviders = true;
-                            var result = usersService.getUsers(ignoreAdminUsers, provider, allProviders, skip, take, search);
+                        var usersRequest;
+                        var specificItemsRequest;
+                        var usersProviderRequest;
 
-                            result.then(function (data) {
+                        ctrl.$scope.usersProviders = [];
+
+                        ctrl.getItems = function (skip, take, search) {
+                            cancelRequest(usersRequest);
+
+                            var provider = ctrl.$scope.sfProvider;
+                            usersRequest = usersService.getUsers(provider, skip, take, search);
+
+                            usersRequest.promise.then(function (data) {
                                 data.Items.map(function (item) {
                                     item.Id = item.UserID;
                                 });
                             });
 
-                            return result;
+                            return usersRequest.promise;
                         };
 
                         ctrl.getSpecificItems = function (ids) {
-                            var provider = ctrl.$scope.sfProvider;
-                            var ignoreAdminUsers = false;
-                            var allProviders = true;
-                            var result = usersService.getSpecificUsers(ids, ignoreAdminUsers, provider, allProviders);
+                            cancelRequest(specificItemsRequest);
 
-                            result.then(function (data) {
+                            specificItemsRequest = usersService.getSpecificUsers(ids);
+
+                            specificItemsRequest.promise.then(function (data) {
                                 data.Items.map(function (item) {
                                     item.Id = item.UserID;
                                 });
                             });
 
-                            return result;
+                            return specificItemsRequest.promise;
+                        };
+
+                        ctrl.$scope.providerChanged = function (provider) {
+                            cancelRequest(usersRequest);
+                            ctrl.$scope.showLoadingIndicator = true;
+                            ctrl.$scope.sfProvider = provider;
+                            ctrl.$scope.paging.skip = 0;
+                            ctrl.$scope.paging.areAllItemsLoaded = false;
+
+                            var endlessScroll = angular.element($("[sf-endless-scroll]"))[0];
+                            if (endlessScroll) {
+                                endlessScroll.scrollTop = 0;
+                            }
+
+                            ctrl.getItems(ctrl.$scope.paging.skip, ctrl.$scope.paging.take, ctrl.$scope.filter.searchString)
+                                .then(onItemsLoadedSuccess, ctrl.onError);
+                        };
+
+                        ctrl.onResetItems = function () {
+                            if (ctrl.$scope.usersProviders && ctrl.$scope.usersProviders.length > 0) {
+                                ctrl.$scope.sfProvider = ctrl.$scope.usersProviders[0].UserProviderName;
+                            }
+                        };
+
+                        ctrl.onCancel = function () {
+                            cancelRequest(usersRequest);
+                            cancelRequest(specificItemsRequest);
+                            cancelRequest(usersProviderRequest);
+                        };
+
+                        ctrl.onDoneSelecting = function () {
+                            cancelRequest(usersRequest);
+                            cancelRequest(specificItemsRequest);
+                            cancelRequest(usersProviderRequest);
+                        };
+
+                        ctrl.onOpen = function () {
+                            if (!ctrl.$scope.usersProviders || ctrl.$scope.usersProviders.length === 0) {
+                                loadUsersProviders();
+                            }
+                        };
+
+                        ctrl.canPushSelectedItemFirst = function () {
+                            return !ctrl.$scope.sfProvider ||
+                                    (ctrl.$scope.selectedItemsInTheDialog && ctrl.$scope.selectedItemsInTheDialog.length > 0 &&
+                                    ctrl.$scope.sfProvider === ctrl.$scope.selectedItemsInTheDialog[0].ProviderName);
+                        };
+
+                        var cancelRequest = function (request) {
+                            if (request && request.cancel) {
+                                request.cancel('canceled');
+                            }
+                        };
+
+                        var onItemsLoadedSuccess = function (data) {
+                            ctrl.$scope.paging.skip += data.Items.length;
+                            ctrl.$scope.items.length = 0;
+
+                            if (ctrl.$scope.selectedItemsInTheDialog && ctrl.$scope.selectedItemsInTheDialog.length > 0) {
+                                ctrl.$scope.sfSelectedItems = [ctrl.$scope.selectedItemsInTheDialog[0]];
+                                ctrl.$scope.sfSelectedItem = ctrl.$scope.selectedItemsInTheDialog[0];
+                            }
+
+                            if (!ctrl.$scope.multiselect && !ctrl.$scope.filter.searchString && ctrl.canPushSelectedItemFirst()) {
+                                ctrl.pushSelectedItemToTheTop(data.Items);
+                                ctrl.pushNotSelectedItems(data.Items);
+                            }
+                            else {
+                                ctrl.$scope.items = data.Items;
+                            }
+
+                            ctrl.$scope.showLoadingIndicator = false;
+
+                            return ctrl.$scope.items;
+                        };
+
+                        var loadUsersProviders = function () {
+                            usersProviderRequest = usersService.getUserProviders();
+                            usersProviderRequest.promise.then(function (data) {
+                                ctrl.$scope.usersProviders = data.Items;
+
+                                if (ctrl.$scope.usersProviders && ctrl.$scope.usersProviders.length > 0) {
+                                    ctrl.$scope.sfProvider = ctrl.$scope.usersProviders[0].UserProviderName;
+                                }
+                            });
                         };
 
                         ctrl.selectorType = 'UserSelector';
