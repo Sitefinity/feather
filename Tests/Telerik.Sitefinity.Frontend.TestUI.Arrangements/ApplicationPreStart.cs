bf6781cb58d659d9c01773e64b63be413f6c9074
sdimitrov89 <stoyan.dimitrov@telerik.com>
2015-06-01 18:40:23 +0300

    Fix for ui tests arrangemets

---
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/ApplicationPreStart.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceModel.Activation;
+using System.Web.Routing;
+using Telerik.Sitefinity.TestUI.Arrangements.Framework;
+
+namespace Telerik.Sitefinity.Frontend.TestUI.Arrangements
+{
+    /// <summary>
+    /// This class is used for setting up the Core project before the application has been started
+    /// </summary>
+    public static class ApplicationPreStart
+    {
+        /// <summary>
+        /// The method that initializes all the preparations
+        /// </summary>
+        public static void Init()
+        {
+            if (!RouteTable.Routes.Contains(ApplicationPreStart.UITestsServiceRoute))
+            {
+                RouteTable.Routes.Add("ui-tests", ApplicationPreStart.UITestsServiceRoute);
+            }
+        }
+
+        internal static readonly ServiceRoute UITestsServiceRoute =
+            new ServiceRoute(TestsArrangemetsService.UiTestsWebServiceUrl, new WebServiceHostFactory(), typeof(TestsArrangemetsService));
+    }
+}
