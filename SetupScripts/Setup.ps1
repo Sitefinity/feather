5e15b6c353c27dd092369730f3b2cb76d3a398b2
sdimitrov89 <stoyan.dimitrov@telerik.com>
2015-06-02 11:03:11 +0300

    Delete SetupScripts folder

--- SetupScripts/Setup.ps1
+++
@@ -1,5 +0,0 @@
-. "$PSScriptRoot\Config.ps1"
-. "$PSScriptRoot\SitefinitySetup.ps1" -useBlobSite $false -enableFeatherModule $true -rebuildSitefinity $false
-
-Write-Output "Closing left IE processes..."
-Get-Process iexplore -ErrorAction Ignore | Stop-Process
