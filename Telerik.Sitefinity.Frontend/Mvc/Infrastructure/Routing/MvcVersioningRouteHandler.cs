b0d39ff1a886d7a517eb57b6547f4762903c07f4
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-06-04 11:11:13 +0300

    Fixed blank screen in revision history

---
+++ Telerik.Sitefinity.Frontend/Mvc/Infrastructure/Routing/MvcVersioningRouteHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Routing;
+using Telerik.Sitefinity.Web;
+
+namespace Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Routing
+{
+    /// <summary>
+    /// Extended route handler for Sitefinity versioning that injects MVC specific logic.
+    /// </summary>
+    public class MvcVersioningRouteHandler : VersioningRouteHandler
+    {
+        /// <summary>
+        /// Provides the object that processes the request.
+        /// </summary>
+        /// <param name="requestContext">An object that encapsulates information about the request.</param>
+        /// <returns>An object that processes the request.</returns>
+        public override IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            var handler = base.GetHttpHandler(requestContext);
+            new PageInitializer().Initialize(handler);
+
+            return handler;
+        }
+    }
+}
