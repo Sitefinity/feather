d4f8c98582fed3a0f95108d1f5762cc8a03036ea
GeorgiMateev <georgi.mateev@telerik.com>
2015-06-09 17:14:59 +0300

    Added detail page id param

--- Telerik.Sitefinity.Frontend/Mvc/Controllers/IRelatedDataController.cs
+++ Telerik.Sitefinity.Frontend/Mvc/Controllers/IRelatedDataController.cs
@@ -15,6 +15,8 @@ namespace Telerik.Sitefinity.Frontend.Mvc.Controllers
         /// Action that returns view with the related items of the given item.
         /// </summary>
         /// <param name="relatedItem">The related item.</param>
+        /// <param name="detailPageId">The id of the page where the detail of the item will be shown.</param>
+        /// <param name="openInSamePage">Determines whether the detail of the item will be on the same page or in a other one.</param>
         /// <param name="templateName">Name of the template.</param>
         /// <param name="relatedDataViewModel">The related data view model. Contains settings needed to retrieve related data.</param>
         /// <param name="settingsViewModel">The settings view model. The widget's model will be initialized with them.</param>
@@ -22,6 +24,8 @@ namespace Telerik.Sitefinity.Frontend.Mvc.Controllers
         /// <returns></returns>
         ActionResult RelatedData(
             IDataItem relatedItem,
+            Guid detailPageId,
+            bool openInSamePage,
             string templateName,
             RelatedDataViewModel relatedDataViewModel,
             ContentListSettingsViewModel settingsViewModel,
