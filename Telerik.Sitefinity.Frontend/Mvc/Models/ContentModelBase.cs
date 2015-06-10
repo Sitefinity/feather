9df8da45340cddfc5b297f334470c3f6fcb1924f
GeorgiMateev <georgi.mateev@telerik.com>
2015-06-10 14:46:18 +0300

    Fixed an error in the code where page number is not passed to a method

--- Telerik.Sitefinity.Frontend/Mvc/Models/ContentModelBase.cs
+++ Telerik.Sitefinity.Frontend/Mvc/Models/ContentModelBase.cs
@@ -280,7 +280,7 @@ namespace Telerik.Sitefinity.Frontend.Mvc.Models
                 throw new ArgumentNullException("relatedItem");
 
             int? totalCount = 0;
-            var query = this.GetRelatedItems(relatedItem, 1, ref totalCount);
+            var query = this.GetRelatedItems(relatedItem, page, ref totalCount);
 
             var viewModel = this.CreateListViewModelInstance();
 
