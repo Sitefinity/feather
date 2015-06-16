3f041e9b4e582e50d455db3d60b97f9c9fedefff
VenetaBogoeva <veneta.bogoeva@telerik.com>
2015-06-16 17:46:41 +0300

    change set up scripts

--- SetupScripts/Cleanup.bat
+++
@@ -1,4 +0,0 @@
-set agentTempFolder="C:\Users\sfbuild\AppData\Local\VSEQT\QTAgent"
-set aspNetTempFolder="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files\root"
-
-"%windir%\sysnative\WindowsPowerShell\v1.0\powershell.exe" -Command "Start-Process PowerShell -windowstyle hidden -ArgumentList '& Start-Sleep 30;$qtAgentServiceProcess = Get-Process QTAgentService;$qtAgentServiceProcess.Kill();$qtAgentServiceProcess.WaitForExit();Cmd /C \"rmdir /S /Q %agentTempFolder%\";Cmd /C \"rmdir /S /Q %aspNetTempFolder%\"'"
--- Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/Properties/AssemblyInfo.cs
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/Properties/AssemblyInfo.cs
@@ -1,7 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Web;
 using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes;
+using Telerik.Sitefinity.Mvc.TestUI.Arrangements;
 using Telerik.Sitefinity.TestUI.Arrangements.Core.Framework.Attributes;
 
 // General Information about an assembly is controlled through the following 
@@ -12,5 +14,6 @@ using Telerik.Sitefinity.TestUI.Arrangements.Core.Framework.Attributes;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ea1806b3-fb25-4b38-afd4-307184cdfa6a")]
 
+[assembly: PreApplicationStartMethod(typeof(ApplicationPreStart), "Init")]
 [assembly: UIArrangement]
 [assembly: ControllerContainer] 
