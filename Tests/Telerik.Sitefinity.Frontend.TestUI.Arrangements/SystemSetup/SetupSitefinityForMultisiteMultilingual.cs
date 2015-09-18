7d8e5983655fac0eaed2a230a168d8c48d53cfb7
VenetaBogoeva <veneta.bogoeva@telerik.com>
2015-09-18 13:45:56 +0300

    fix site creation

--- Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/SystemSetup/SetupSitefinityForMultisiteMultilingual.cs
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.Arrangements/SystemSetup/SetupSitefinityForMultisiteMultilingual.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using Telerik.Sitefinity.TestArrangementService.Attributes;
+using Telerik.Sitefinity.TestArrangementService.Core;
 using Telerik.Sitefinity.TestIntegration.Helpers;
 using Telerik.Sitefinity.TestUI.Arrangements.Framework;
 using Telerik.Sitefinity.TestUtilities.CommonOperations;
+using Telerik.Sitefinity.TestUtilities.Helpers.Models;
 
 namespace Telerik.Sitefinity.Frontend.TestUI.Arrangements
 {
@@ -20,7 +22,11 @@ namespace Telerik.Sitefinity.Frontend.TestUI.Arrangements
         public void CreateMultilingualSite()
         {  
             AuthenticationHelper.AuthenticateUser(Admin, Password);
-            MultisiteHelper.CreateSite(SiteName, Url, Cultures, SiteName + "Provider");
+            var siteName = ArrangementConfig.GetArrangementSite();
+            var siteUrl = ArrangementConfig.GetArrangementSiteUrl();
+            var siteCultures = ArrangementConfig.GetArrangementSiteCultures();
+            var site = new SiteModel(siteName, siteUrl, siteName + "Provider", true) { Cultures = siteCultures };
+            MultisiteHelper.CreateSite(site);
         }
 
         /// <summary>
@@ -34,6 +40,7 @@ namespace Telerik.Sitefinity.Frontend.TestUI.Arrangements
 
         private const string SiteName = "SecondSite";
         private const string Url = "http://localhost:83/";
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
         private static readonly List<string> Cultures = new List<string>() { "en", "bg-bg" };
         private const string Admin = "admin";
         private const string Password = "admin@2";
