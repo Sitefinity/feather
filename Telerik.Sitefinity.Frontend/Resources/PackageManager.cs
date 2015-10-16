7149cadbf96541a6e03977c06f03006ec2c4e35b
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-10-16 15:41:31 +0300

    Reverted setting of based on column of templates

--- Telerik.Sitefinity.Frontend/Resources/PackageManager.cs
+++ Telerik.Sitefinity.Frontend/Resources/PackageManager.cs
@@ -116,11 +116,25 @@ namespace Telerik.Sitefinity.Frontend.Resources
         }
 
         /// <summary>
+        /// Gets the key of the edited container.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <remarks>That is page template Id when editing page template or message body Id when editing Email Campaigns issue.</remarks>
+        /// <returns>The key.</returns>
+        private static string GetEditedContainerKey(HttpContextBase context)
+        {
+            var requestContext = context.Items[RouteHandler.RequestContextKey] as RequestContext ?? context.Request.RequestContext;
+            var keys = requestContext.RouteData.Values["Params"] as string[];
+
+            return (keys != null && keys.Length > 0) ? keys[0] : null;
+        }
+
+        /// <summary>
         /// Gets the package from template.
         /// </summary>
         /// <param name="template">The template.</param>
         /// <returns></returns>
-        internal string GetPackageFromTemplate(PageTemplate template)
+        private string GetPackageFromTemplate(PageTemplate template)
         {
             var currentTemplate = template;
             while (currentTemplate != null)
@@ -145,20 +159,6 @@ namespace Telerik.Sitefinity.Frontend.Resources
             }
 
             return null;
-        }
-
-        /// <summary>
-        /// Gets the key of the edited container.
-        /// </summary>
-        /// <param name="context">The context.</param>
-        /// <remarks>That is page template Id when editing page template or message body Id when editing Email Campaigns issue.</remarks>
-        /// <returns>The key.</returns>
-        private static string GetEditedContainerKey(HttpContextBase context)
-        {
-            var requestContext = context.Items[RouteHandler.RequestContextKey] as RequestContext ?? context.Request.RequestContext;
-            var keys = requestContext.RouteData.Values["Params"] as string[];
-
-            return (keys != null && keys.Length > 0) ? keys[0] : null;
         }
 
         /// <summary>
