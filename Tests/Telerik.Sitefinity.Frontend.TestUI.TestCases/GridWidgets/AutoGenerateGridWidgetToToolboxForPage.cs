061e4e86374e1f4c02c4a0a116b4eee51582305c
VenetaBogoeva <veneta.bogoeva@telerik.com>
2015-10-21 20:31:26 +0300

    fix UI test

--- Tests/Telerik.Sitefinity.Frontend.TestUI.TestCases/GridWidgets/AutoGenerateGridWidgetToToolboxForPage.cs
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.TestCases/GridWidgets/AutoGenerateGridWidgetToToolboxForPage.cs
@@ -25,7 +25,6 @@ namespace Telerik.Sitefinity.Frontend.TestUI.TestCases.GridWidgets
             RuntimeSettingsModificator.ExecuteWithClientTimeout(800000, () =>  BAT.Macros().NavigateTo().CustomPage("~/sitefinity/pages", false));
             RuntimeSettingsModificator.ExecuteWithClientTimeout(800000, () => BAT.Macros().User().EnsureAdminLoggedIn());
             RuntimeSettingsModificator.ExecuteWithClientTimeout(800000, () => BAT.Macros().NavigateTo().CustomPage("~/sitefinity/pages", false));
-            RuntimeSettingsModificator.ExecuteWithClientTimeout(800000, () => BAT.Macros().User().EnsureAdminLoggedIn());
             BAT.Wrappers().Backend().Pages().PagesWrapper().OpenPageZoneEditor(PageName);
             BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().SwitchEditorLayoutMode(EditorLayoutMode.Layout);            
             BATFrontend.Wrappers().Backend().Pages().PageZoneEditorWrapper().DragAndDropLayoutWidgetToPlaceholder(LayoutCaption);
