6e5bd3cef4aaba29124f809c08d16f65e517fb64
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-11-20 10:57:00 +0200

    Added event on startup

---
+++ Telerik.Sitefinity.Frontend/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Telerik.Sitefinity.Abstractions;
+using Telerik.Sitefinity.Modules.Pages;
+using Telerik.Sitefinity.Mvc.Store;
+using Telerik.Sitefinity.Services;
+
+namespace Telerik.Sitefinity.Frontend
+{
+    /// <summary>
+    /// Contains the application startup event handles related to the Feather functionality of Sitefinity.
+    /// </summary>
+    public static class Startup
+    {
+        /// <summary>
+        /// Called before the Asp.Net application is started.
+        /// </summary>
+        public static void OnPreApplicationStart()
+        {
+            Bootstrapper.Initialized += Bootstrapper_Initialized;
+        }
+
+        private static void Bootstrapper_Initialized(object sender, Data.ExecutedEventArgs e)
+        {
+            if (e.CommandName == "Bootstrapped")
+            {
+                EventHub.Subscribe<IWidgetValidatingEvent>(WidgetValidating);
+            }
+        }
+
+        private static void WidgetValidating(IWidgetValidatingEvent evt)
+        {
+            var featherModule = SystemManager.GetModule("Feather");
+            if (featherModule == null)
+            {
+                evt.IsValid = ControllerStore.Controllers().Any(c => c.ControllerType.FullName == evt.WidgetTypeFullName);
+            }
+        }
+    }
+}
