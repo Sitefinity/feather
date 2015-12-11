9f1d379dd70c67d66684ec4f5dfec3169502982b
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-12-11 15:38:44 +0200

    Ensured logged in the beginning of tests

--- Tests/Telerik.Sitefinity.Frontend.TestUI.TestCases/GridWidgets/AutoGenerateGridWidgetToToolboxForPageTemplate.cs
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.TestCases/GridWidgets/AutoGenerateGridWidgetToToolboxForPageTemplate.cs
@@ -22,8 +22,8 @@ namespace Telerik.Sitefinity.Frontend.TestUI.TestCases.GridWidgets
         TestCategory(FeatherTestCategories.PagesAndContent)]
         public void AddAndRenameGridWidgetFromFileSystemVerifyTemplateToolbox()
         {                        
+            RuntimeSettingsModificator.ExecuteWithClientTimeout(800000, () => BAT.Macros().User().EnsureAdminLoggedIn());
             RuntimeSettingsModificator.ExecuteWithClientTimeout(800000, () => BAT.Macros().NavigateTo().CustomPage("~/sitefinity/design/pagetemplates", false));
-            RuntimeSettingsModificator.ExecuteWithClientTimeout(800000, () => BAT.Macros().User().EnsureAdminLoggedIn());              
             BAT.Wrappers().Backend().PageTemplates().PageTemplateMainScreen().OpenTemplateEditor(PageTemplateName);
             BAT.Wrappers().Backend().Pages().PageZoneEditorWrapper().SwitchEditorLayoutMode(EditorLayoutMode.Layout);            
             BATFrontend.Wrappers().Backend().Pages().PageZoneEditorWrapper().DragAndDropLayoutWidgetToPlaceholder(LayoutCaption);
