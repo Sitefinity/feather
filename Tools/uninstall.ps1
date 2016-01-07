c9a9e4eb325113bcf04e29c3c4d48e327e30eca7
ElenaGaneva <Elena.Ganeva@telerik.com>
2016-01-07 17:06:33 +0200

    Added tools folder with ps scripts for nuget install

---
+++ Tools/uninstall.ps1
@@ -0,0 +1,13 @@
+param($installPath, $toolsPath, $package, $project)
+
+ # This is the MSBuild targets file to add
+    $targetsFile = [System.IO.Path]::Combine($toolsPath, 'RazorGenerator.MsBuild.targets')
+
+    # Need to load MSBuild assembly if it's not loaded yet.
+    Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+    # Grab the loaded MSBuild project for the project
+    $msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+
+    # Add the import and save the project
+    $msbuild.Xml.AddImport("Build\RazorGenerator.MsBuild\build\RazorGenerator.MsBuild.targets") | out-null
+    $project.Save()
