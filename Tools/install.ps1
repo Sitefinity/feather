feb8978d706a04e9af9f8404e27adc0a1458c9fa
ElenaGaneva <Elena.Ganeva@telerik.com>
2016-01-11 12:08:45 +0200

    Removed double slash from ps nuget script

--- Tools/install.ps1
+++ Tools/install.ps1
@@ -4,6 +4,6 @@ param($installPath, $toolsPath, $package, $project)
   Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
   # Grab the loaded MSBuild project for the project
   $msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
-  $importToRemove = $msbuild.Xml.Imports | Where-Object { $_.Project -eq 'Build\\RazorGenerator.MsBuild\\build\\RazorGenerator.MsBuild.targets' -or $_.Project -eq 'Build\\FeatherPrecompilation.targets'} | %{ $msbuild.Xml.RemoveChild($_) }
+  $importToRemove = $msbuild.Xml.Imports | Where-Object { $_.Project -eq 'Build\RazorGenerator.MsBuild\build\RazorGenerator.MsBuild.targets' -or $_.Project -eq 'Build\FeatherPrecompilation.targets'} | %{ $msbuild.Xml.RemoveChild($_) }
   
   $project.Save()
