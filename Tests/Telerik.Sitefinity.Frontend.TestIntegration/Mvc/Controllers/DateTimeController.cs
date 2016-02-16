9d019a5d94a16d543fd9bf453951e67eb2f86ccd
ElenaGaneva <Elena.Ganeva@telerik.com>
2016-02-16 16:50:11 +0200

    Added integration test for outputcahce of mvc widget

---
+++ Tests/Telerik.Sitefinity.Frontend.TestIntegration/Mvc/Controllers/DateTimeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Telerik.Sitefinity.Frontend.TestIntegration.Mvc.Controllers
+{
+    /// <summary>
+    /// This is dummy date time controller.
+    /// </summary>
+    public class DateTimeController : Controller
+    {
+        public static int Count { get; set; }
+
+        /// <summary>
+        /// This is the default Action.
+        /// </summary>
+        public ActionResult Index()
+        {
+            DateTimeController.Count++;
+
+            return this.Content(DateTime.Now.ToString());
+        }
+    }
+}
