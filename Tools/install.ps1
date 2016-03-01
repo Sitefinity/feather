c4c128cbe86dbf2c7db9d86d6d532ff4fe11d97a
Boyko Karadzhov <bokovskii@gmail.com>
2016-03-01 15:20:36 +0200

    nuget now adds controllercontainer attribute

--- Tools/install.ps1
+++ Tools/install.ps1
@@ -34,3 +34,22 @@ param($installPath, $toolsPath, $package, $project)
   if (Test-Path $recaptchaTemplatesPath) {
     Remove-Item $recaptchaTemplatesPath -Recurse -Confirm
   }
+
+  Write-Host "Appending ControllerContainerAttribute to the AssemblyInfo..."
+  $assemblyInfoPath = Join-Path $projectDirectory "Properties\AssemblyInfo.cs"
+  if (Test-Path $assemblyInfoPath)
+  {
+    if ((Get-Content $assemblyInfoPath | ? { $_ -match "\[.*assembly.*\:.*ControllerContainer.*\]" } | group).Count -eq 0)
+    {
+      Add-Content $assemblyInfoPath "[assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ControllerContainer]"
+      Write-Host "Finished appending ControllerContainerAttribute to the AssemblyInfo."
+    }
+    Else
+    {
+      Write-Host "ControllerContainerAttribute is already in the AssemblyInfo. Nothing is appended."
+    }
+  }
+  Else
+  {
+    Write-Host "AssemblyInfo not found."
+  }
