1322d0f9b501899006d24fc9dc0d5a4b7f71b95d
VenetaBogoeva <veneta.bogoeva@telerik.com>
2016-04-21 14:02:20 +0300

    Added UI test EditLayoutClassOfGridWidget

--- Tests/Telerik.Sitefinity.Frontend.TestUI.Framework/Framework/Wrappers/Backend/Widgets/GridWidgets.cs
+++ Tests/Telerik.Sitefinity.Frontend.TestUI.Framework/Framework/Wrappers/Backend/Widgets/GridWidgets.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ArtOfTest.WebAii.Controls.HtmlControls;
+using ArtOfTest.WebAii.Core;
 
 namespace Telerik.Sitefinity.Frontend.TestUI.Framework.Wrappers.Backend
 {
@@ -22,6 +23,40 @@ namespace Telerik.Sitefinity.Frontend.TestUI.Framework.Wrappers.Backend
             gridButton.ScrollToVisible();
             gridButton.Focus();
             gridButton.MouseClick();
+        }
+
+        /// <summary>
+        /// Sets the custom CSS to grid widget.
+        /// </summary>
+        /// <param name="css">The CSS.</param>
+        public void SetCustomCssToGridWidget(string css)
+        {
+            HtmlInputText input = this.EM.Widgets.GridWidgets.CustomBodyText
+                                      .AssertIsPresent("input");
+
+            input.ScrollToVisible();
+            input.Focus();
+            input.MouseClick();
+
+            Manager.Current.Desktop.KeyBoard.KeyDown(System.Windows.Forms.Keys.Control);
+            Manager.Current.Desktop.KeyBoard.KeyPress(System.Windows.Forms.Keys.A);
+            Manager.Current.Desktop.KeyBoard.KeyUp(System.Windows.Forms.Keys.Control);
+            Manager.Current.Desktop.KeyBoard.KeyPress(System.Windows.Forms.Keys.Delete);
+
+            Manager.Current.Desktop.KeyBoard.TypeText(css);
+        }
+
+        /// <summary>
+        /// Clicks the save button.
+        /// </summary>
+        public void ClickSaveButton()
+        {
+            HtmlButton saveButton = this.EM.Widgets.GridWidgets.SaveButton
+                                        .AssertIsPresent("save button");
+
+            saveButton.Click();
+            ActiveBrowser.WaitForAsyncRequests();
+            ActiveBrowser.RefreshDomTree();
         }
     }
 }
