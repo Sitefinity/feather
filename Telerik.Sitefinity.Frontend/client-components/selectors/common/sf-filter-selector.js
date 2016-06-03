3a9514fc6c0a871f1a009068d55c6a6a667aa93f
mknijarova <milena.knijarova@gmail.com>
2016-06-03 12:16:01 +0300

    Fix #141533 Feather: When select 'Any time' in Date selector - the selected option is not saved in News widget

--- Telerik.Sitefinity.Frontend/client-components/selectors/common/sf-filter-selector.js
+++ Telerik.Sitefinity.Frontend/client-components/selectors/common/sf-filter-selector.js
@@ -10,7 +10,7 @@
                 },
                 templateUrl: function (elem, attrs) {
                     var assembly = attrs.sfTemplateAssembly || 'Telerik.Sitefinity.Frontend';
-                    var url = attrs.sfTemplateUrl || 'client-components/selectors/common/sf-filter-selector.html';
+                    var url = attrs.sfTemplateUrl || 'client-components/selectors/common/sf-filter-selector.sf-cshtml';
                     return sitefinity.getEmbeddedResourceUrl(assembly, url);
                 },
                 link: {
