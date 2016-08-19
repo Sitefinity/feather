6e7e28e72b6fda54400701d959849d3fe9937bba
TsvetanRazsolkov <tsvetan.razsolkov@gmail.com>
2016-08-19 17:16:28 +0300

    SocialShareHelpers.SocialShareOptions overload added without data item as parameter.

--- Telerik.Sitefinity.Frontend/Mvc/Helpers/SocialShareHelpers.cs
+++ Telerik.Sitefinity.Frontend/Mvc/Helpers/SocialShareHelpers.cs
@@ -11,6 +11,26 @@ namespace Telerik.Sitefinity.Frontend.Mvc.Helpers
         /// Socials the share options. Redirect to the SocialShare control if exist else render error message
         /// </summary>
         /// <param name="helper">The HTML helper.</param>
+        public static System.Web.Mvc.MvcHtmlString SocialShareOptions(this HtmlHelper helper)
+        {
+            System.Web.Mvc.MvcHtmlString result;
+            try
+            {
+                result = helper.Action(ActionName, ControllerName);
+            }
+            catch (HttpException)
+            {
+                result = new System.Web.Mvc.MvcHtmlString("The SocialShare widget could not be found.");
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Socials the share options. Redirect to the SocialShare control if exist else render error message
+        /// </summary>
+        /// <param name="helper">The HTML helper.</param>
+        /// <param name="dataItem">The data item which we will be sharing</param>
         public static System.Web.Mvc.MvcHtmlString SocialShareOptions(this HtmlHelper helper, Telerik.Sitefinity.Model.IHasTitle dataItem)
         {
             System.Web.Mvc.MvcHtmlString result;
