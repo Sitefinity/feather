1ad765cbb9d40c3bce6720d5f79fdea8df80174a
gg3orgiev <gigeorgiev@progress.com>
2017-01-03 16:43:50 +0200

    Update AssemblyCopyright for 2017.

--- AssemblyInfoShare/SharedAssemblyInfo.cs
+++ AssemblyInfoShare/SharedAssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyCompany("Telerik")]
 [assembly: AssemblyProduct("Telerik Sitefinity CMS")]
-[assembly: AssemblyCopyright("Copyright © 2005-2016 Telerik AD. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2005-2017 Telerik AD. All rights reserved.")]
 [assembly: AssemblyTrademark("Sitefinity")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
