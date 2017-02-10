9dabcb9c7faae09c60ea63158f83da076cad18fa
Nikolay Dobrev <nikydobrev@gmail.com>
2017-02-10 11:06:25 +0200

    Updated version to 1.7.690.0

--- AssemblyInfoShare/SharedAssemblyInfo.cs
+++ AssemblyInfoShare/SharedAssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyCompany("Telerik")]
@@ -9,19 +9,19 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2005-2017 Telerik AD. All rights reserved.")]
 [assembly: AssemblyTrademark("Sitefinity")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.7.680.0")]
-[assembly: AssemblyFileVersion("1.7.680.0")]
+[assembly: AssemblyVersion("1.7.690.0")]
+[assembly: AssemblyFileVersion("1.7.690.0")]
