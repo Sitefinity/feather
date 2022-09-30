99f5384785c37a59ddd18e631a24362c9a82c89a
Abhijeet2019 <47414994+Abhijeet2019@users.noreply.github.com>
2022-09-30 16:01:31 +0530

    Solve bug multiple selector in same designer view
    Bug in Sitefinity having multiple sf-media-selector in same designer view (document/videos) always update first sf-media-selector element.
    Also bug present when having autoOpenSelector attribute presents always open first sf-media-selector.

--- Telerik.Sitefinity.Frontend/client-components/fields/media-field/sf-media-field.js
+++ Telerik.Sitefinity.Frontend/client-components/fields/media-field/sf-media-field.js
@@ -117,7 +117,9 @@
                     scope.mediaItemDeleted = false;
 
                     var autoOpenSelector = attrs.sfAutoOpenSelector !== undefined && attrs.sfAutoOpenSelector.toLowerCase() !== 'false';
-
+                    
+                    var mediaElement = element;
+                    
                     var mediaType = sfMediaTypeResolver.get(scope.sfMediaType);
 
                     var getDateFromString = function (dateStr) {
@@ -277,7 +279,18 @@
                             scope.model.filterObject.status = attrs.sfMaster === 'true' || attrs.sfMaster === 'True' ? 'master' : 'live';
                         }
 
-                        var mediaSelectorModalScope = angular.element('.mediaSelectorModal').scope();
+                        var mediaSelector = angular.element('.mediaSelectorModal');
+
+                        //Add below lines of code to solve multiple sf-media-field selector in same designer view
+                        if (mediaElement && mediaElement.length > 0) {
+                            var el = mediaElement[0].querySelector('.mediaSelectorModal');
+                            if (el) {
+                                var ngEl = angular.element(el);
+                                if (ngEl) {
+                                    mediaSelector = ngEl;
+                                }
+                            }
+                        }
 
                         if (mediaSelectorModalScope)
                             mediaSelectorModalScope.$openModalDialog();
